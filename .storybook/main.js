391cb8baed54486aee76f18d5f6e242b2a199624
berdysheva <berdysheva@yandex-team.ru>
2021-11-01 18:23:47 +0300

    Try fix css-imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,14 +3,17 @@ const path = require('path');
 const root = path.join(__dirname, '..');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const {ConfigBuilder, javascript, styles, assets} = require('@yandex-data-ui/webpack-levels');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const srcRoot = path.resolve(root, 'src');
 const stylesRoot = path.resolve(root, 'styles');
 const assetsRoot = path.resolve(root, 'assets');
+const commonRoot = path.resolve(__dirname, '..', 'node_modules/@yandex-data-ui/common');
+const docToolsRoot = path.resolve(__dirname, '..', 'node_modules/@doc-tools');
 
 const storybookRoot = path.resolve(root, '.storybook');
 
-const ruleIncludes = [srcRoot, stylesRoot, assetsRoot, storybookRoot];
+const ruleIncludes = [srcRoot, stylesRoot, assetsRoot, storybookRoot, commonRoot, docToolsRoot];
 
 const config = new ConfigBuilder();
 
@@ -44,6 +47,21 @@ config
             spriteFilename: 'sprite-[hash:6].svg',
         },
     })
+    .module.addRule({
+        test: /\.s?css$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'sass-loader',
+                // options: {
+                //     sassOptions: {
+                //         indentedSyntax: true,
+                //     }
+                // }
+            },
+        ],
+    })
     .plugins.addPlugin(new SpriteLoaderPlugin({plainSprite: true}));
 
 const projectConfig = config.build();
@@ -76,6 +94,27 @@ module.exports = {
             test: /\.md$/,
             include: [root],
             use: [{loader: 'html-loader'}, {loader: 'markdown-loader'}],
+        });
+        // storybookBaseConfig.module.rules.push({
+        //     test: /\.s?css$/,
+        //     use:[
+        //         'style-loader',
+        //         'css-loader',
+        //         {
+        //             loader: 'sass-loader',
+        //             options: {
+        //                 sassOptions: {
+        //                     indentedSyntax: true
+        //                 }
+        //             }
+        //         }
+        //     ]
+        // });
+        storybookBaseConfig.module.rules.push({
+            test: /\.(ico|bmp|gif|jpe?g|png|svg)$/,
+            loader: 'url-loader',
+            include: [root],
+            exclude: [path.resolve(assetsRoot, 'icons')],
         });
 
         // без этого fileName в context.parameters в продакшн сборке становится цифрой, а не путём, и ссылку на сорсы не сформировать
