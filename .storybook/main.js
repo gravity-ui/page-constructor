12b232019d2009a3fa368d57529ad9601c70122f
berdysheva <berdysheva@yandex-team.ru>
2021-11-03 16:04:52 +0300

    Remove comments

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,13 @@ const path = require('path');
 const root = path.join(__dirname, '..');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 const {ConfigBuilder, javascript, styles, assets} = require('@yandex-data-ui/webpack-levels');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const srcRoot = path.resolve(root, 'src');
 const stylesRoot = path.resolve(root, 'styles');
 const assetsRoot = path.resolve(root, 'assets');
-// const commonRoot = path.resolve(__dirname, '..', 'node_modules/@yandex-data-ui/common');
-// const docToolsRoot = path.resolve(__dirname, '..', 'node_modules/@doc-tools');
 
 const storybookRoot = path.resolve(root, '.storybook');
 
-// const ruleIncludes = [srcRoot, stylesRoot, assetsRoot, storybookRoot, commonRoot, docToolsRoot];
 const ruleIncludes = [srcRoot, stylesRoot, assetsRoot, storybookRoot];
 
 const config = new ConfigBuilder();
@@ -44,21 +40,6 @@ config
             spriteFilename: 'sprite-[hash:6].svg',
         },
     })
-    // .module.addRule({
-    //     test: /\.s?css$/,
-    //     use: [
-    //         'style-loader',
-    //         'css-loader',
-    //         {
-    //             loader: 'sass-loader',
-    //             // options: {
-    //             //     sassOptions: {
-    //             //         indentedSyntax: true,
-    //             //     }
-    //             // }
-    //         },
-    //     ],
-    // })
     .plugins.addPlugin(new SpriteLoaderPlugin({plainSprite: true}));
 
 const projectConfig = config.build();
@@ -92,21 +73,6 @@ module.exports = {
             include: [root],
             use: [{loader: 'html-loader'}, {loader: 'markdown-loader'}],
         });
-        // storybookBaseConfig.module.rules.push({
-        //     test: /\.s?css$/,
-        //     use:[
-        //         'style-loader',
-        //         'css-loader',
-        //         {
-        //             loader: 'sass-loader',
-        //             options: {
-        //                 sassOptions: {
-        //                     indentedSyntax: true
-        //                 }
-        //             }
-        //         }
-        //     ]
-        // });
         storybookBaseConfig.module.rules.push({
             test: /\.(ico|bmp|gif|jpe?g|png|svg)$/,
             loader: 'url-loader',
