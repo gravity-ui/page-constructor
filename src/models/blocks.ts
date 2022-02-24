8e129cd5d44bb9be4f09a47ee0d47738cef0400f
berdysheva <berdysheva@yandex-team.ru>
2022-02-24 15:21:47 +0300

    Remove cards-with-image block [CLOUDFRONT-9555]

--- src/models/blocks.ts
+++ src/models/blocks.ts
@@ -61,7 +61,6 @@ export enum BlockType {
     SimpleBlock = 'simple-block',
     HeaderSliderBlock = 'header-slider-block',
     LinkTableBlock = 'link-table-block',
-    CardsWithImageBlock = 'cards-with-image-block',
     HeaderBlock = 'header-block',
     IconsBlock = 'icons-block',
     CardLayoutBlock = 'card-layout-block',
@@ -747,13 +746,6 @@ export interface CardWithImageProps extends ClassNameProps {
     }[];
 }
 
-export interface CardsWithImageBlockProps {
-    title: TitleProps | string;
-    cards: CardWithImageProps[];
-    description?: string;
-    colSizes?: GridColumnSizesType;
-}
-
 export interface CardLayoutBlockProps extends Childable, Animatable, LoadableChildren {
     title: TitleProps | string;
     description?: string;
@@ -997,10 +989,6 @@ export type LinkTableBlockModel = {
     type: BlockType.LinkTableBlock;
 } & LinkTableBlockProps;
 
-export type CardsWithImageBlockModel = {
-    type: BlockType.CardsWithImageBlock;
-} & CardsWithImageBlockProps;
-
 export type CardLayoutBlockModel = {
     type: BlockType.CardLayoutBlock;
 } & CardLayoutBlockProps;
@@ -1060,7 +1048,6 @@ type BlockV2Raw =
     | TextTableBlockModel
     | SimpleBlockModel
     | LinkTableBlockModel
-    | CardsWithImageBlockModel
     | HeaderBlockModel
     | PreviewBlockModel
     | IconsBlockModel
