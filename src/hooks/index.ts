f4dfb353b190ea67707673d479bf4d7c8abc8858
Vladislav M. Kharitonov <vladvlad@yandex-team.ru>
2022-04-13 15:45:41 +0300

    export hooks from package (#116)
    Co-authored-by: Vladislav Kharitonov <vladvlad@sandbox-407922969>

---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export {default as useFocus} from './useFocus';
+export {default as useWindowBreakpoint} from './useWindowBreakpoint';
+export {useIntersection} from './useIntersection';
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from './components';
 export * from './models';
 export * from './utils';
 export * from './schema';
+export * from './hooks';
