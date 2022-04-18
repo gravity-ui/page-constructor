b72827194b4e66a66eb78814589383a58adcdd7e
Aleksandra Pleshcheva <aeksandla@yandex-team.ru>
2022-04-18 12:51:22 +0000

    Merge pull request #6 in DATA-UI/page-constructor from aeksandla/CLOUDFRONT-10232/fix-extended-features-link to master
    Squashed commit of the following:
    
    commit a59cc503539475ab6a741ce79303d519976111b8
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Mon Apr 18 15:41:04 2022 +0300
    
        Add link to stories [CLOUDFRONT-10232]
    
    commit a64ca161f02318a27055cb97a295b1d4b0f58fec
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Mon Apr 18 15:30:00 2022 +0300
    
        Add link to stories [CLOUDFRONT-10232]
    
    commit 8626ad0249ef96b1de13dd6495ead8d89fd0df8c
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Mon Apr 18 11:02:37 2022 +0300
    
        Update story  [CLOUDFRONT-10232]
    
    commit 897d0fceee7fd2f1606d7b559f2ed9ede7f14be0
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Mon Apr 18 10:55:00 2022 +0300
    
        Fix link in ExtendedFeatures  [CLOUDFRONT-10024]

--- src/blocks/ExtendedFeatures/__stories__/ExtendedFeatures.stories.tsx
+++ src/blocks/ExtendedFeatures/__stories__/ExtendedFeatures.stories.tsx
@@ -22,16 +22,34 @@ Default.args = {
             title: 'Грант на знакомство с платформой',
             text: '<a href="/docs/free-trial/">Грант на знакомство с платформой</a> — не менее 4000 рублей — вы получите уже при создании платёжного аккаунта. Эту сумму можно тратить на облачные сервисы Yandex.Cloud в течение двух месяцев.',
             icon: 'https://storage.yandexcloud.net/cloud-www-assets/constructor/prices/rub.svg',
+            link: {
+                text: 'Перейти',
+                url: '#',
+                arrow: true,
+                theme: 'normal',
+            },
         },
         {
             title: 'Выгодные тарифы и free tier',
             text: 'От 116 рублей в месяц за аренду виртуальной машины, от 530 рублей — за кластер управляемой базы данных. Небольшой сайт, приложение или сервис можно разместить бесплатно, используя <a href="/docs/billing/concepts/serverless-free-tier">free tier экосистемы бессерверных вычислений</a>.',
             icon: 'https://storage.yandexcloud.net/cloud-www-assets/constructor/prices/calc.svg',
+            link: {
+                text: 'Перейти',
+                url: '#',
+                arrow: true,
+                theme: 'normal',
+            },
         },
         {
             title: 'Прозрачное ценообразование и скидки',
             text: 'В Yandex.Cloud вы всегда знаете, сколько потратите и как будет расходоваться бюджет. Зарезервируйте ресурсы заранее — на 1 или 3 года, и сэкономьте до 49%. Если уверены в прогнозе нагрузок, <a href="#contact-form">напишите в отдел продаж</a>.',
             icon: 'https://storage.yandexcloud.net/cloud-www-assets/constructor/prices/proc.svg',
+            link: {
+                text: 'Перейти',
+                url: '#',
+                arrow: true,
+                theme: 'normal',
+            },
         },
     ],
 };
