c56ffa1ec72cd493ba2d670cf81878e322ee81d7
VladVlad <vladvlad@yandex-team.ru>
2022-04-19 11:33:53 +0300

    refactor card base to functional component

--- src/components/CardBase/CardBase.tsx
+++ src/components/CardBase/CardBase.tsx
@@ -24,86 +24,93 @@ export interface CardFooterBaseProps {
     className?: string;
 }
 
+interface LayoutParts {
+    Header: React.FC<CardHeaderBaseProps>;
+    Content: React.FC;
+    Footer: React.FC<CardFooterBaseProps>;
+}
+
 const b = block('card-base-block');
+
 const Header: React.FC<CardHeaderBaseProps> = () => null;
 const Content: React.FC = () => null;
 const Footer: React.FC<CardFooterBaseProps> = () => null;
 
-export default class Layout extends React.Component<CardBaseProps> {
-    static Header = Header;
-    static Content = Content;
-    static Footer = Footer;
+export const Layout: React.FC<CardBaseProps> & LayoutParts = (props) => {
+    const {
+        className,
+        bodyClassName,
+        contentClassName,
+        children,
+        url,
+        target,
+        border = 'shadow',
+        backgroundColor,
+    } = props;
+    let header, content, footer, image, headerClass, footerClass;
 
-    render() {
-        const {
-            className,
-            bodyClassName,
-            contentClassName,
-            children,
-            url,
-            target,
-            border = 'shadow',
-            backgroundColor,
-        } = this.props;
-        let header, content, footer, image, headerClass, footerClass;
-
-        function handleChild(child: ReactElement) {
-            switch (child.type) {
-                case Header:
-                    header = child.props.children;
-                    image = child.props.image;
-                    headerClass = child.props.className;
-                    break;
-                case Content:
-                    content = child.props.children;
-                    break;
-                case Footer:
-                    footer = child.props.children;
-                    footerClass = child.props.className;
-                    break;
-            }
+    function handleChild(child: ReactElement) {
+        switch (child.type) {
+            case Header:
+                header = child.props.children;
+                image = child.props.image;
+                headerClass = child.props.className;
+                break;
+            case Content:
+                content = child.props.children;
+                break;
+            case Footer:
+                footer = child.props.children;
+                footerClass = child.props.className;
+                break;
         }
-
-        if (Children.count(children) === 1) {
-            handleChild(children as ReactElement);
-        } else {
-            Children.forEach(children, handleChild);
-        }
-
-        const cardContent = (
-            <Fragment>
-                {(header || image) && (
-                    <BackgroundImage
-                        className={b('header', headerClass)}
-                        {...(typeof image === 'string' ? {src: image} : image)}
-                    >
-                        <div className={b('header-content')}>{header}</div>
-                    </BackgroundImage>
-                )}
-                <div className={b('body', bodyClassName)}>
-                    <div className={b('content', contentClassName)}>{content}</div>
-                    {footer && <div className={b('footer', footerClass)}>{footer}</div>}
-                </div>
-            </Fragment>
-        );
-
-        const fullClassName = b({border: border}, className);
-
-        return url ? (
-            <a
-                href={url}
-                target={target}
-                rel={target === '_blank' ? 'noopener noreferrer' : undefined}
-                className={fullClassName}
-                draggable={false}
-                onDragStart={(e) => e.preventDefault()}
-            >
-                {cardContent}
-            </a>
-        ) : (
-            <div className={fullClassName} style={{backgroundColor}}>
-                {cardContent}
-            </div>
-        );
     }
-}
+
+    if (Children.count(children) === 1) {
+        handleChild(children as ReactElement);
+    } else {
+        Children.forEach(children, handleChild);
+    }
+
+    const cardContent = (
+        <Fragment>
+            {(header || image) && (
+                <BackgroundImage
+                    className={b('header', headerClass)}
+                    {...(typeof image === 'string' ? {src: image} : image)}
+                >
+                    <div className={b('header-content')}>{header}</div>
+                </BackgroundImage>
+            )}
+            <div className={b('body', bodyClassName)}>
+                <div className={b('content', contentClassName)}>{content}</div>
+                {footer && <div className={b('footer', footerClass)}>{footer}</div>}
+            </div>
+        </Fragment>
+    );
+
+    const fullClassName = b({border: border}, className);
+
+    return url ? (
+        <a
+            href={url}
+            target={target}
+            rel={target === '_blank' ? 'noopener noreferrer' : undefined}
+            className={fullClassName}
+            draggable={false}
+            onDragStart={(e) => e.preventDefault()}
+        >
+            {cardContent}
+        </a>
+    ) : (
+        <div className={fullClassName} style={{backgroundColor}}>
+            {cardContent}
+        </div>
+    );
+};
+
+Layout.Header = Header;
+Layout.Content = Content;
+Layout.Footer = Footer;
+
+export default Layout;
--- src/context/locationContext/locationContext.ts
+++ src/context/locationContext/locationContext.ts
@@ -1,12 +1,19 @@
 import React from 'react';
 import {History} from '../mobileAppContext';
 
+export interface RouterLinkProps {
+    href: string;
+}
+
+export type RouterLink = React.ComponentClass<RouterLinkProps>;
+
 export type LocationContextProps = {
     history?: History;
     search?: string;
     hash?: string;
     pathname?: string;
     hostname?: string;
+    Link?: RouterLink;
 };
 
 export const LocationContext = React.createContext<LocationContextProps>({});
