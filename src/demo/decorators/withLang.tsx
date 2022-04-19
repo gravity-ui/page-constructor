e6b98b2d2b5cb36661e56c323957fffc349058be
berdysheva <berdysheva@yandex-team.ru>
2022-04-19 19:31:08 +0300

    Revert using withMobile [ORION-695]

--- src/demo/decorators/withLang.tsx
+++ src/demo/decorators/withLang.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Story as StoryType, StoryContext} from '@storybook/react/types-6-0';
 import {I18N} from '@yandex-cloud/i18n';
 
+I18N.setDefaultLang(I18N.LANGS.ru);
+
 export function withLang(Story: StoryType, context: StoryContext) {
     const lang = context.globals.lang || 'ru';
     I18N.setDefaultLang(lang);
