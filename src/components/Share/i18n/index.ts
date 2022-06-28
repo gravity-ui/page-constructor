878d4aa48404667390d4d903dc415dd22fd28662
VladVlad <vladvlad@yandex-team.ru>
2022-06-28 11:00:25 +0300

    add i18n configuring to project

--- src/components/Share/i18n/index.ts
+++ src/components/Share/i18n/index.ts
@@ -1,12 +1,13 @@
-import {i18n, I18N} from '../../../i18n';
+import {i18n} from '../../../i18n';
+import {Lang} from '../../../configure';
 
 const COMPONENT = 'Share';
 
-i18n.registerKeyset(I18N.LANGS.en, COMPONENT, {
+i18n.registerKeyset(Lang.En, COMPONENT, {
     'constructor-share': 'Share',
 });
 
-i18n.registerKeyset(I18N.LANGS.ru, COMPONENT, {
+i18n.registerKeyset(Lang.Ru, COMPONENT, {
     'constructor-share': 'Поделиться',
 });
 
--- src/components/UnpublishedLabel/i18n/index.ts
+++ src/components/UnpublishedLabel/i18n/index.ts
@@ -1,12 +1,13 @@
-import {i18n, I18N} from '../../../i18n';
+import {i18n} from '../../../i18n';
+import {Lang} from '../../../configure';
 
 const COMPONENT = 'UnpublishedLabel';
 
-i18n.registerKeyset(I18N.LANGS.en, COMPONENT, {
+i18n.registerKeyset(Lang.En, COMPONENT, {
     label_non_published: 'Unpublished',
 });
 
-i18n.registerKeyset(I18N.LANGS.ru, COMPONENT, {
+i18n.registerKeyset(Lang.Ru, COMPONENT, {
     label_non_published: 'Не опубликовано',
 });
 
