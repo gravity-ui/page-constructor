163b189efae6fc8efbe18f4a71e74a27b0f570b0
Aleksandra Pleshcheva <aeksandla@yandex-team.ru>
2022-07-19 10:47:25 +0000

    Add yaml snippet [CLOUDFRONT-10923]
    Squashed commit of the following:
    
    commit da8fb701f26f9c1eb1364b08a10e9e3f355a2aa6
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Mon Jul 18 16:20:29 2022 +0300
    
        Fix addons [CLOUDFRONT-10923]
    
    commit bc3a5382c8655564c4e6a94a3fd3788decb30dd9
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Tue Jul 12 13:56:28 2022 +0300
    
        Fix naming [CLOUDFRONT-10923]
    
    commit d1f37fe1f1e1aa09f37be53b8fa84a35f3e29147
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Tue Jul 12 13:31:17 2022 +0300
    
        Update yaml.dump options [CLOUDFRONT-10923]
    
    commit 058d7f74bd03418a5a03a3cf9137aa19154baae7
    Author: aeksandla <aeksandla@yandex-team.ru>
    Date:   Tue Jul 12 12:54:45 2022 +0300
    
        Add yaml snippet to storybook [CLOUDFRONT-10923]

--- .storybook/main.js
+++ .storybook/main.js
@@ -52,7 +52,11 @@ const projectConfig = config.build();
 
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-knobs'],
+    addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-knobs',
+        './addons/addon-yaml/preset.js',
+    ],
     typescript: {
         check: true,
         checkOptions: {},
