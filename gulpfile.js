d40366ac0a5896a3f73b7ba5a1cdfdfa79bca327
VladVlad <vladvlad@yandex-team.ru>
2022-08-16 19:54:05 +0300

    build ui-core -> gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,18 @@ const rimraf = require('rimraf');
 const ts = require('gulp-typescript');
 const replace = require('gulp-replace');
 const sass = require('gulp-dart-sass');
+const rename = require('gulp-rename');
 
-const BUILD_DIR = path.resolve('build');
+const BUILD_CLIENT_DIR = path.resolve('build');
+const BUILD_SERVER_DIRS = {
+    server: path.resolve('server'),
+    configure: path.resolve('configure'),
+};
 
 task('clean', (done) => {
-    rimraf.sync(BUILD_DIR);
+    rimraf.sync(BUILD_CLIENT_DIR);
+    rimraf.sync(BUILD_SERVER_DIRS.server);
+    rimraf.sync(BUILD_SERVER_DIRS.configure);
     rimraf.sync('styles/**/*.css');
     done();
 });
@@ -23,8 +30,8 @@ function getStylesTasks() {
         task(taskName, () => {
             return src([`src/${dir}/**/*.scss`, `!src/${dir}/**/__stories__/**/*.scss`])
                 .pipe(sass().on('error', sass.logError))
-                .pipe(dest(path.resolve(BUILD_DIR, 'esm', dir)))
-                .pipe(dest(path.resolve(BUILD_DIR, 'cjs', dir)));
+                .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'esm', dir)))
+                .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'cjs', dir)));
         });
 
         return taskName;
@@ -42,6 +49,8 @@ function compileTs(modules = false) {
         '!src/demo/**/*.{js,jsx,ts,tsx}',
         '!src/stories/**/*.{js,jsx,ts,tsx}',
         '!src/**/__stories__/**/*.{js,jsx,ts,tsx}',
+        '!src/server.ts',
+        '!src/configure.ts',
     ])
         .pipe(
             replace(/import '.+\.scss';/g, (match) =>
@@ -49,7 +58,23 @@ function compileTs(modules = false) {
             ),
         )
         .pipe(tsProject())
-        .pipe(dest(path.resolve(BUILD_DIR, modules ? 'esm' : 'cjs')));
+        .pipe(dest(path.resolve(BUILD_CLIENT_DIR, modules ? 'esm' : 'cjs')));
+}
+
+function compileServerTs(entry) {
+    const tsProject = ts.createProject(`tsconfig.${entry}.json`, {
+        declaration: true,
+        module: 'commonjs',
+    });
+
+    return src([`src/**/${entry}.ts`])
+        .pipe(tsProject())
+        .pipe(
+            rename(function (filePath) {
+                filePath.basename = filePath.basename.replace(entry, 'index');
+            }),
+        )
+        .pipe(dest(path.resolve(BUILD_SERVER_DIRS[entry])));
 }
 
 task('compile-to-esm', () => {
@@ -60,6 +85,14 @@ task('compile-to-cjs', () => {
     return compileTs();
 });
 
+task('compile-server', () => {
+    return compileServerTs('server');
+});
+
+task('compile-configure', () => {
+    return compileServerTs('configure');
+});
+
 task('copy-js-declarations', () => {
     return src([
         'src/**/*.d.ts',
@@ -67,14 +100,14 @@ task('copy-js-declarations', () => {
         '!src/stories/**/*.d.ts',
         '!src/**/__stories__/**/*.d.ts',
     ])
-        .pipe(dest(path.resolve(BUILD_DIR, 'esm')))
-        .pipe(dest(path.resolve(BUILD_DIR, 'cjs')));
+        .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'esm')))
+        .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'cjs')));
 });
 
 task('copy-i18n', () => {
     return src(['src/**/i18n/*.json'])
-        .pipe(dest(path.resolve(BUILD_DIR, 'esm')))
-        .pipe(dest(path.resolve(BUILD_DIR, 'cjs')));
+        .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'esm')))
+        .pipe(dest(path.resolve(BUILD_CLIENT_DIR, 'cjs')));
 });
 
 task('styles-global', () => {
@@ -85,7 +118,7 @@ task(
     'build',
     series([
         'clean',
-        parallel(['compile-to-esm', 'compile-to-cjs']),
+        parallel(['compile-to-esm', 'compile-to-cjs', 'compile-server', 'compile-configure']),
         'copy-js-declarations',
         'copy-i18n',
         parallel(['styles-global', ...getStylesTasks()]),
