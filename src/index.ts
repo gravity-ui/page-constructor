f19b52705652f3a83312cca2e4920fc8554bbc65
ngorin <ngorin@yandex-team.ru>
2022-09-01 11:11:21 +0300

    add exports in index ORION-1464

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-//storybook
-export * from './contexts/theme';
+export {configure} from './configure';
 
-// export * from './models';
-// export * from './utils';
+export {BlogConstructorProvider} from './constructor/BlogConstructorProvider';
+
+export {BlogPostPage} from './containers/BlogPostPage/BlogPostPage';
+
+export {transformPageContent} from './data/transformPageContent';
+export {createReadableContent} from './data/createReadableContent';
+export {sanitizeMeta} from './data/sanitizeMeta';
+export {transformPost} from './data/transformPost';
+
+export * from './models/blog';
+export * from './models/common';
+export * from './models/locale';
 
 export {BREAKPOINTS} from './constants';
