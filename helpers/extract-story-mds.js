dcea4d1231125296231547c074833ec5f720f0f6
VladVlad <vladvlad@yandex-team.ru>
2022-09-24 11:14:41 +0300

    add mds docs and stories transltation content

--- helpers/extract-story-mds.js
+++ helpers/extract-story-mds.js
@@ -4,12 +4,17 @@ const path = require('path');
 
 const RES_DIRNAME = 'readmes';
 const resultDirName = path.join(__dirname, RES_DIRNAME);
+const EN_READMES_DIR = './translated/readmes-en/';
+const EN_STORIES_CONTENT_DIR = './translated/stories-content_en/';
 
-createDir(resultDirName)
-extractFiles();
+
+// createDir(resultDirName)
+// extractFiles();
+// placeTranslatedReadmes(EN_READMES_DIR);
+placeTranslatedReadmes(EN_STORIES_CONTENT_DIR, 'json');
 
 function extractFiles(ext = 'md') {
-    return glob(`./**/*.${ext}`,{
+    return glob(`./**/*.${ext}`, {
         ignore:'./node_modules/**/*.md'
     }, (er, files) => {
         if(er) {
@@ -37,4 +42,32 @@ function createDir(name) {
     }
 
     fs.mkdirSync(resultDirName);
+}
+
+function placeTranslatedReadmes(readmesDir, ext = 'md') {
+    glob(`${readmesDir}/*${ext}`, {}, (er, files) => {
+
+        if(er) {
+            throw Error(er.message);
+        }   
+
+        files.forEach((fileName) => {
+            const filePathRegexp = new RegExp(`\\.(?!${ext})`, 'g')
+            const targetPath = fileName
+                .replace('.stories(en)','_content')
+                .replace(readmesDir, '')
+                .replace(filePathRegexp,'/');
+
+            fs.copyFile(fileName, targetPath,(err) => {
+                if(err) {
+                    console.error(`Unable to copy file ${fileName}`, err)
+                } else {
+                    console.error(`File ${fileName} copied`)
+                }
+                
+            })
+
+            
+        });
+    })
 }
