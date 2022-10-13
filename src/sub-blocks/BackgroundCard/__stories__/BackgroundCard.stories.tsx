50d5eafe10594e8394f6efa86ab66c0f2e46dffa
berdysheva <berdysheva@yandex-team.ru>
2022-10-13 15:41:40 +0300

    fix(Content): ul li color in dark theme

--- src/sub-blocks/BackgroundCard/__stories__/BackgroundCard.stories.tsx
+++ src/sub-blocks/BackgroundCard/__stories__/BackgroundCard.stories.tsx
@@ -1,8 +1,9 @@
 import {Meta, Story} from '@storybook/react/types-6-0';
 import React from 'react';
+import yfm from '@doc-tools/transform';
 
 import BackgroundCard from '../BackgroundCard';
-import {BackgroundCardModel, BackgroundCardProps} from '../../../models';
+import {BackgroundCardProps} from '../../../models';
 import {CARDS, COMPONENTS} from '../../../demo/constants';
 
 import data from './data.json';
@@ -46,7 +47,15 @@ const CardThemesTemplate: Story<BackgroundCardProps> = (args) => (
             <BackgroundCard {...args} title="Тема по умолчанию = s" theme="default" />
         </div>
         <div style={{maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="Темная тема = m" theme="dark" />
+            <BackgroundCard
+                {...args}
+                title="Темная тема = m"
+                theme="dark"
+                background={{
+                    ...args.buttons,
+                    src: 'https://storage.yandexcloud.net/cloud-www-assets/constructor/storybook/images/img-black.png',
+                }}
+            />
         </div>
         <div style={{maxWidth: '400px', padding: '0 8px'}}>
             <BackgroundCard {...args} title="Светлая тема = l" theme="light" />
@@ -62,10 +71,43 @@ export const BorderLine = DefaultTemplate.bind({});
 export const BackgroundColor = DefaultTemplate.bind({});
 export const WithTheme = DefaultTemplate.bind({});
 
-Default.args = data.default.content as BackgroundCardModel;
-WithBackgroundImage.args = data.withBackgroundImage.content as BackgroundCardModel;
-Paddings.args = data.paddings.content as BackgroundCardModel;
-CardThemes.args = data.cardThemes.content as BackgroundCardModel;
-BorderLine.args = data.borderLine.content as BackgroundCardModel;
-BackgroundColor.args = data.backgroundColor.content as BackgroundCardModel;
-WithTheme.args = data.withTheme.content as BackgroundCardModel;
+const DefaultArgs = {
+    ...data.default.content,
+    text: yfm(data.default.content.text).result.html,
+    additionalInfo: yfm(data.default.content.additionalInfo).result.html,
+};
+
+Default.args = {
+    ...DefaultArgs,
+} as BackgroundCardProps;
+
+WithBackgroundImage.args = {
+    ...DefaultArgs,
+    ...data.withBackgroundImage.content,
+} as BackgroundCardProps;
+
+Paddings.args = {
+    ...DefaultArgs,
+    ...data.withBackgroundImage.content,
+} as BackgroundCardProps;
+
+CardThemes.args = {
+    ...DefaultArgs,
+    ...data.withBackgroundImage.content,
+} as BackgroundCardProps;
+
+BorderLine.args = {
+    ...DefaultArgs,
+    ...data.borderLine.content,
+    ...data.withBackgroundImage.content,
+} as BackgroundCardProps;
+
+BackgroundColor.args = {
+    ...DefaultArgs,
+    ...data.backgroundColor.content,
+} as BackgroundCardProps;
+
+WithTheme.args = {
+    ...DefaultArgs,
+    ...data.withTheme.content,
+} as BackgroundCardProps;
