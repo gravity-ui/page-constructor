b6a9205e0cbadc1961c70c7be1fd40a3c371cf31
berdysheva <berdysheva@yandex-team.ru>
2022-10-13 19:41:59 +0300

    fix: fix storybook data

--- src/sub-blocks/BackgroundCard/__stories__/BackgroundCard.stories.tsx
+++ src/sub-blocks/BackgroundCard/__stories__/BackgroundCard.stories.tsx
@@ -8,6 +8,9 @@ import {CARDS, COMPONENTS} from '../../../demo/constants';
 
 import data from './data.json';
 
+const getPaddingBottomTitle = (padding: string) =>
+    data.paddings.title.replace('{{padding}}', padding);
+
 export default {
     component: BackgroundCard,
     title: `${COMPONENTS}/${CARDS}/BackgroundCard`,
@@ -27,46 +30,36 @@ const DefaultTemplate: Story<BackgroundCardProps> = (args) => (
 const PaddingsTemplate: Story<BackgroundCardProps> = (args) => (
     <div style={{display: 'flex'}}>
         <div style={{display: 'inline-table', maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="paddingBottom = s" paddingBottom="s" />
+            <BackgroundCard {...args} title={getPaddingBottomTitle('S')} paddingBottom="s" />
         </div>
         <div style={{display: 'inline-table', maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="paddingBottom = m" paddingBottom="m" />
+            <BackgroundCard {...args} title={getPaddingBottomTitle('M')} paddingBottom="m" />
         </div>
         <div style={{display: 'inline-table', maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="paddingBottom = l" paddingBottom="l" />
+            <BackgroundCard {...args} title={getPaddingBottomTitle('L')} paddingBottom="l" />
         </div>
         <div style={{display: 'inline-table', maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="paddingBottom = xl" paddingBottom="xl" />
+            <BackgroundCard {...args} title={getPaddingBottomTitle('XL')} paddingBottom="xl" />
         </div>
     </div>
 );
 
-const CardThemesTemplate: Story<BackgroundCardProps> = (args) => (
-    <div style={{display: 'flex'}}>
-        <div style={{maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="Тема по умолчанию = s" theme="default" />
+const CardThemesTemplate: Story<BackgroundCardProps[]> = (args) => {
+    return (
+        <div style={{display: 'flex'}}>
+            {Object.values(args).map((item, i) => (
+                <div style={{maxWidth: '400px', padding: '0 8px'}} key={i}>
+                    <BackgroundCard {...item} />
+                </div>
+            ))}
         </div>
-        <div style={{maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard
-                {...args}
-                title="Темная тема = m"
-                theme="dark"
-                background={{
-                    ...args.buttons,
-                    src: 'https://storage.yandexcloud.net/cloud-www-assets/constructor/storybook/images/img-black.png',
-                }}
-            />
-        </div>
-        <div style={{maxWidth: '400px', padding: '0 8px'}}>
-            <BackgroundCard {...args} title="Светлая тема = l" theme="light" />
-        </div>
-    </div>
-);
+    );
+};
 
 export const Default = DefaultTemplate.bind({});
 export const WithBackgroundImage = DefaultTemplate.bind({});
 export const Paddings = PaddingsTemplate.bind({});
-export const CardThemes = CardThemesTemplate.bind({});
+export const CardThemes = CardThemesTemplate.bind([]);
 export const BorderLine = DefaultTemplate.bind({});
 export const BackgroundColor = DefaultTemplate.bind({});
 export const WithTheme = DefaultTemplate.bind({});
@@ -91,10 +84,10 @@ Paddings.args = {
     ...data.withBackgroundImage.content,
 } as BackgroundCardProps;
 
-CardThemes.args = {
+CardThemes.args = [...data.cardThemes.content].map((item) => ({
     ...DefaultArgs,
-    ...data.withBackgroundImage.content,
-} as BackgroundCardProps;
+    ...item,
+})) as BackgroundCardProps[];
 
 BorderLine.args = {
     ...DefaultArgs,
