1c57b40c2695eab28ca1aee6cfc90d1a91cf3b40
ngorin <ngorin@yandex-team.ru>
2023-02-22 15:17:15 +0300

    feat: fix ? in regexp

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
     rootDir: '.',
     transform: {
-        '^.+\\.[jt]sx??$': 'ts-jest',
+        '^.+\\.[jt]sx?$': 'ts-jest',
     },
     transformIgnorePatterns: ['node_modules/(?!(@gravity-ui|react-github-btn)/)'],
     coverageDirectory: './coverage',
