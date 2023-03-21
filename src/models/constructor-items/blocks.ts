db53a626f0d54f8748272a569680e9a6426f7fdb
Nik Tverd <niktverd@yandex-team.ru>
2023-03-21 09:00:42 +0600

    feat: delete Preview block BREAKING CHANGE: deletion of Preview block from the package

--- src/models/constructor-items/blocks.ts
+++ src/models/constructor-items/blocks.ts
@@ -16,8 +16,6 @@ import {
     MediaDirection,
     MediaProps,
     MapProps,
-    PreviewItemProps,
-    PreviewRatioMediaContent,
     TextSize,
     TextTheme,
     ThemedImage,
@@ -45,7 +43,6 @@ export enum BlockType {
     BannerBlock = 'banner-block',
     CompaniesBlock = 'companies-block',
     MediaBlock = 'media-block',
-    PreviewBlock = 'preview-block',
     InfoBlock = 'info-block',
     SecurityBlock = 'security-block',
     TableBlock = 'table-block',
@@ -238,16 +235,6 @@ export interface MapBlockProps extends MediaBaseBlockProps {
     map: MapProps;
 }
 
-export interface PreviewBlockProps extends Animatable {
-    title: string;
-    items: PreviewItemProps[];
-    description?: string;
-    direction?: MediaDirection;
-    ratioMediaContent?: PreviewRatioMediaContent;
-    stopVideo?: boolean;
-    showImmediately?: boolean;
-}
-
 export interface InfoBlockProps {
     theme?: TextTheme;
     backgroundColor?: ThemeSupporting<string>;
@@ -435,10 +422,6 @@ export type MapBlockModel = {
     type: BlockType.MapBlock;
 } & MapBlockProps;
 
-export type PreviewBlockModel = {
-    type: BlockType.PreviewBlock;
-} & PreviewBlockProps;
-
 export type InfoBlockModel = {
     type: BlockType.InfoBlock;
 } & InfoBlockProps;
@@ -500,7 +483,6 @@ type BlockModels =
     | TabsBlockModel
     | LinkTableBlockModel
     | HeaderBlockModel
-    | PreviewBlockModel
     | IconsBlockModel
     | HeaderSliderBlockModel
     | CardLayoutBlockModel
