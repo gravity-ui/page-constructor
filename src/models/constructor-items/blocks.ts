a94156c9fdfb92df75e6b018ba9e8544b2da9910
Nik Tverd <niktverd@yandex-team.ru>
2023-03-24 19:01:10 +0600

    Revert "feat: delete Preview block"
    This reverts commit a10f3f44d5cfd95bb2aa21d158ed4130fc8308df.

--- src/models/constructor-items/blocks.ts
+++ src/models/constructor-items/blocks.ts
@@ -43,6 +43,7 @@ export enum BlockType {
     BannerBlock = 'banner-block',
     CompaniesBlock = 'companies-block',
     MediaBlock = 'media-block',
+    PreviewBlock = 'preview-block',
     InfoBlock = 'info-block',
     SecurityBlock = 'security-block',
     TableBlock = 'table-block',
@@ -232,6 +233,16 @@ export interface MapBlockProps extends MediaBaseBlockProps {
     map: MapProps;
 }
 
+export interface PreviewBlockProps extends Animatable {
+    title: string;
+    items: PreviewItemProps[];
+    description?: string;
+    direction?: MediaDirection;
+    ratioMediaContent?: PreviewRatioMediaContent;
+    stopVideo?: boolean;
+    showImmediately?: boolean;
+}
+
 export interface InfoBlockProps {
     theme?: TextTheme;
     backgroundColor?: ThemeSupporting<string>;
@@ -411,6 +422,10 @@ export type MapBlockModel = {
     type: BlockType.MapBlock;
 } & MapBlockProps;
 
+export type PreviewBlockModel = {
+    type: BlockType.PreviewBlock;
+} & PreviewBlockProps;
+
 export type InfoBlockModel = {
     type: BlockType.InfoBlock;
 } & InfoBlockProps;
@@ -470,6 +485,7 @@ type BlockModels =
     | TabsBlockModel
     | LinkTableBlockModel
     | HeaderBlockModel
+    | PreviewBlockModel
     | IconsBlockModel
     | HeaderSliderBlockModel
     | CardLayoutBlockModel
