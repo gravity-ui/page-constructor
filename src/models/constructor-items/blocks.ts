9bbaa49cd8f2edce6b9379c05d85def37b8c8eef
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2023-03-24 11:58:11 +0300

    feat(SimpleBlock): remove simple-block (#228)

--- src/models/constructor-items/blocks.ts
+++ src/models/constructor-items/blocks.ts
@@ -49,7 +49,6 @@ export enum BlockType {
     SecurityBlock = 'security-block',
     TableBlock = 'table-block',
     TabsBlock = 'tabs-block',
-    SimpleBlock = 'simple-block',
     HeaderSliderBlock = 'header-slider-block',
     LinkTableBlock = 'link-table-block',
     HeaderBlock = 'header-block',
@@ -163,11 +162,6 @@ export interface HeaderBlockProps {
 }
 
 export type CalculatorProps = Animatable;
-
-export interface SimpleBlockProps extends Animatable, Childable {
-    title: TitleBaseProps;
-    description: string;
-}
 
 export interface ExtendedFeaturesItem
     extends Omit<ContentBlockProps, 'theme' | 'centered' | 'colSizes' | 'size' | 'title'> {
@@ -447,10 +441,6 @@ export type TabsBlockModel = {
     type: BlockType.TabsBlock;
 } & TabsBlockProps;
 
-export type SimpleBlockModel = {
-    type: BlockType.SimpleBlock;
-} & SimpleBlockProps;
-
 export type LinkTableBlockModel = {
     type: BlockType.LinkTableBlock;
 } & LinkTableBlockProps;
@@ -494,7 +484,6 @@ type BlockModels =
     | SecurityBlockModel
     | TableBlockModel
     | TabsBlockModel
-    | SimpleBlockModel
     | LinkTableBlockModel
     | HeaderBlockModel
     | IconsBlockModel
