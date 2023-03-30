94b60283ebc8f48c2005571c251b0e0bc67fca08
Nik Tverd <61203447+niktverd@users.noreply.github.com>
2023-03-30 15:56:15 +0600

    fix(ReactPlayer): transform youtube url for single video src (#262) (#264)

--- src/components/ReactPlayer/utils.ts
+++ src/components/ReactPlayer/utils.ts
@@ -3,21 +3,23 @@ const LIVE_YUOTUBE_VIDEO_REGEX =
     /(?:youtu\.be\/live\/|youtube(?:-nocookie)?\.com\/(?:live\/))((\w|-){11})/;
 const YOUTUBE_VIDEO_TEMPLATE = 'https://www.youtube.com/watch?v=';
 
-export const checkYoutubeVideos = (src: string | string[]) => {
-    if (Array.isArray(src)) {
-        return src.map((videoUrl) => {
-            if (LIVE_YUOTUBE_VIDEO_REGEX.test(videoUrl)) {
-                const youtubeLiveId = videoUrl.match(LIVE_YUOTUBE_VIDEO_REGEX)?.[1];
-                if (!youtubeLiveId) {
-                    return videoUrl;
-                }
+const transformYoutubeUrl = (src: string) => {
+    if (LIVE_YUOTUBE_VIDEO_REGEX.test(src)) {
+        const youtubeLiveId = src.match(LIVE_YUOTUBE_VIDEO_REGEX)?.[1];
+        if (!youtubeLiveId) {
+            return src;
+        }
 
-                return `${YOUTUBE_VIDEO_TEMPLATE}${youtubeLiveId}`;
-            }
-
-            return videoUrl;
-        });
+        return `${YOUTUBE_VIDEO_TEMPLATE}${youtubeLiveId}`;
     }
 
     return src;
+};
+
+export const checkYoutubeVideos = (src: string | string[]) => {
+    if (Array.isArray(src)) {
+        return src.map((videoUrl) => transformYoutubeUrl(videoUrl));
+    }
+
+    return transformYoutubeUrl(src);
 };
