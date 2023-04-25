4395f8a78489ee254dfa25051d8f812a7ed0bf4b
Nik Tverd <61203447+niktverd@users.noreply.github.com>
2023-04-25 16:13:09 +0600

    fix(QuestionBlock): fix itemprop parameter value for FaqPage microdata (#316)

--- src/blocks/Questions/Questions.tsx
+++ src/blocks/Questions/Questions.tsx
@@ -15,7 +15,7 @@ const FaqMicrodataValues = {
     PageType: 'https://schema.org/FAQPage',
     QuestionType: 'https://schema.org/Question',
     QuestionProp: 'mainEntity',
-    QuestionTextProp: 'name',
+    QuestionTextProp: 'text',
     AnswerType: 'https://schema.org/Answer',
     AnswerProp: 'acceptedAnswer',
     AnswerTextProp: 'text',
