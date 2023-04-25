9d96b4c8ecf72829cb42ba5272cfe15bc1edb272
Nikita Gorin <36075690+NikitaCG@users.noreply.github.com>
2023-04-25 11:35:50 +0300

    feat: refactor text-transform functions (#225)
    * feat: refactor cloneDeep sd
    
    * feat: refactor after review
    
    * feat: resolve conflicts after great rebase

---
+++ src/text-transform/common.ts
@@ -0,0 +1,39 @@
+import {Lang} from '../utils/configure';
+import {fullTransform, typografToHTML} from './utils';
+
+export type ComplexItem = {[key: string]: string};
+export type Item = string | null | ComplexItem;
+export type Transformer = (text: string) => string;
+export type TransformerRaw = (lang: Lang, content: string) => string;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Parser<T = any> = (transformer: Transformer, block: T) => T;
+
+export const createItemsParser = (fields: string[]) => (transformer: Transformer, items: Item[]) =>
+    items.map((item) => {
+        if (!item) {
+            return item;
+        } else if (typeof item === 'string') {
+            return transformer(item);
+        } else {
+            return {
+                ...item,
+                ...fields.reduce<ComplexItem>((acc, fieldName) => {
+                    if (item[fieldName]) {
+                        acc[fieldName] = transformer(item[fieldName]);
+                    }
+
+                    return acc;
+                }, {}),
+            };
+        }
+    });
+
+export function yfmTransformer(lang: Lang, content: string) {
+    const {html} = fullTransform(content, {lang});
+
+    return html;
+}
+
+export function typografTransformer(lang: Lang, content: string) {
+    return typografToHTML(content, lang);
+}
---
+++ src/text-transform/transformers.ts
@@ -0,0 +1,89 @@
+import _ from 'lodash';
+
+import {ConstructorBlock} from '../models/constructor';
+import {Lang} from '../utils/configure';
+import {Transformer} from './common';
+import {BlocksConfig, config} from './config';
+import {fullTransform} from './utils';
+
+export type ContentTransformerProps = {
+    content: {
+        blocks?: ConstructorBlock[];
+        footnotes?: string[];
+    };
+    options: {
+        lang: Lang;
+        customConfig?: {};
+    };
+};
+
+function transformBlocks(blocks: ConstructorBlock[], lang: Lang, customConfig = {}) {
+    const fullConfig = {...config, ...customConfig};
+
+    const clonedBlocks = _.cloneDeep(blocks);
+
+    return clonedBlocks.map((block) => transformBlock(lang, fullConfig, block));
+}
+
+function transformBlock(lang: Lang, blocksConfig: BlocksConfig, block: ConstructorBlock) {
+    const blockConfig = blocksConfig[block.type];
+
+    if (block) {
+        if ('randomOrder' in block && block.randomOrder && 'children' in block && block.children) {
+            // eslint-disable-next-line no-not-accumulator-reassign/no-not-accumulator-reassign
+            block.children = _.shuffle(block.children as ConstructorBlock[]);
+        }
+    }
+
+    if (blockConfig) {
+        const configs = Array.isArray(blockConfig) ? blockConfig : [blockConfig];
+
+        configs.forEach((transformConfig) => {
+            const {fields, transformer: transformerRaw, parser} = transformConfig;
+            const transformer: Transformer = transformerRaw.bind(null, lang);
+
+            if (fields) {
+                (fields as (keyof typeof block)[]).forEach((field) => {
+                    if (block[field]) {
+                        if (parser) {
+                            // eslint-disable-next-line no-not-accumulator-reassign/no-not-accumulator-reassign
+                            block[field] = parser(transformer, block[field]);
+                        } else if (typeof block[field] === 'string') {
+                            // eslint-disable-next-line no-not-accumulator-reassign/no-not-accumulator-reassign
+                            block[field] = transformer(block[field]);
+                        }
+                    }
+                });
+            } else if (parser) {
+                parser(transformer, block);
+            }
+        });
+    }
+
+    if ('children' in block && block.children) {
+        // eslint-disable-next-line no-not-accumulator-reassign/no-not-accumulator-reassign
+        block.children = transformBlocks(block.children as ConstructorBlock[], lang, blocksConfig);
+    }
+
+    return block;
+}
+
+export const contentTransformer = ({content, options}: ContentTransformerProps) => {
+    const {lang, customConfig = {}} = options;
+    const {blocks = [], footnotes = []} = content;
+
+    const transformedBlocks = transformBlocks(blocks, lang, customConfig);
+
+    const transformedFootnotes =
+        footnotes
+            ?.map(
+                (footnote) =>
+                    fullTransform(footnote, {path: __dirname, lang, allowHTML: true}).html,
+            )
+            .filter(Boolean) || [];
+
+    return {
+        blocks: transformedBlocks,
+        footnotes: transformedFootnotes,
+    };
+};
