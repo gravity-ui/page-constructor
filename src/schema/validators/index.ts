99c2b0a1422aae76fd74842b6ff358c28fb01e7e
ngorin <ngorin@yandex-team.ru>
2023-04-27 18:44:23 +0300

    feat: add components schemas in export

--- src/schema/validators/index.ts
+++ src/schema/validators/index.ts
@@ -1,5 +1,6 @@
 import * as blocks from './blocks';
 import * as common from './common';
+import * as components from './components';
 import * as navigation from './navigation';
 import * as pixel from './pixel';
 import * as subBlocks from './sub-blocks';
@@ -8,6 +9,7 @@ import * as utils from './utils';
 export const validators = {
     blocks,
     common,
+    components,
     subBlocks,
     pixel,
     utils,
