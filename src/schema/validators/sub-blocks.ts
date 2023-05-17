e404049c846433aaaaaaa99b8ce2cc7613189778
Chris Co <coswarm@gmail.com>
2023-05-17 15:19:53 +0300

    Revert "feat: remove NewsCard"
    This reverts commit 818551d4505cd8b99eb1e849464a2cca211b1c9f.

--- src/schema/validators/sub-blocks.ts
+++ src/schema/validators/sub-blocks.ts
@@ -2,6 +2,7 @@ export * from '../../sub-blocks/PriceDetailed/schema';
 export * from '../../sub-blocks/BackgroundCard/schema';
 export * from '../../sub-blocks/Content/schema';
 export * from '../../sub-blocks/MediaCard/schema';
+export * from '../../sub-blocks/NewsCard/schema';
 export * from '../../sub-blocks/LayoutItem/schema';
 export * from '../../sub-blocks/Quote/schema';
 export * from '../../sub-blocks/Divider/schema';
