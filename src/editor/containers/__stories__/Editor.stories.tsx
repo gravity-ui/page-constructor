8a621a187029c0d8ebaca5a48d68beaaa9e985b6
gorgeousvlad <gorgeousvlad@gmail.com>
2023-06-22 11:44:36 +0200

    feat: add yfm transoform for editor demo

--- src/editor/containers/__stories__/Editor.stories.tsx
+++ src/editor/containers/__stories__/Editor.stories.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import {Meta, Story} from '@storybook/react/types-6-0';
 
 import {scriptsSrc, ymapApiKeyForStorybook} from '../../../../.storybook/maps';
 import {PageConstructor} from '../../../containers/PageConstructor';
+import {LocaleContext} from '../../../context/localeContext';
 import {MapType} from '../../../context/mapsContext/mapsContext';
 import {MapProvider} from '../../../context/mapsContext/mapsProvider';
 import {EDITOR} from '../../../demo/constants';
+import {contentTransformer} from '../../../text-transform';
 import {EditorProps} from '../../types';
 import {Editor} from '../Editor/Editor';
+
+import {memoizeLast} from './utils';
 
 import data from './data.json';
 
@@ -17,15 +21,33 @@ export default {
     component: Editor,
 } as Meta;
 
-const DefaultTemplate: Story<EditorProps> = (args) => (
-    <MapProvider
-        scriptSrc={scriptsSrc[MapType.Yandex]}
-        apiKey={ymapApiKeyForStorybook}
-        type={MapType.Yandex}
-    >
-        <Editor {...args}>{(props) => <PageConstructor {...props} />}</Editor>
-    </MapProvider>
-);
+const contentTransformerMemoized = memoizeLast(contentTransformer);
+
+const DefaultTemplate: Story<EditorProps> = (args) => {
+    const {lang} = useContext(LocaleContext);
+
+    return (
+        <MapProvider
+            scriptSrc={scriptsSrc[MapType.Yandex]}
+            apiKey={ymapApiKeyForStorybook}
+            type={MapType.Yandex}
+        >
+            <Editor {...args}>
+                {({content, ...rest}) => (
+                    <PageConstructor
+                        {...rest}
+                        content={
+                            content && {
+                                ...content,
+                                ...contentTransformerMemoized({content, options: {lang}}),
+                            }
+                        }
+                    />
+                )}
+            </Editor>
+        </MapProvider>
+    );
+};
 
 export const Default = DefaultTemplate.bind({});
 
