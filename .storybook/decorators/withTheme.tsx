6cfdc11d63e09929c0c1c64a27cf44f3288a5930
kkmch <kkmch@yandex-team.ru>
2023-07-20 12:10:29 +0300

    fix: using single source of theme value

--- .storybook/decorators/withTheme.tsx
+++ .storybook/decorators/withTheme.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import {StoryFn, StoryContext} from '@storybook/react';
+import type {StoryFn, StoryContext} from '@storybook/react';
 import {useTheme} from '../../src/context/theme';
 
 export function withTheme(Story: StoryFn, context: StoryContext) {
-    const themeValue = context.globals.theme;
-    const [theme, setTheme] = useTheme(); // eslint-disable-line react-hooks/rules-of-hooks
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    React.useEffect(() => {
-        if (theme !== themeValue) {
-            setTheme(themeValue);
-        }
-    }, [theme, themeValue, setTheme]);
+    const theme = useTheme(); // eslint-disable-line react-hooks/rules-of-hooks
 
     return <Story {...context} />;
 }
--- src/context/theme/ThemeContext.ts
+++ src/context/theme/ThemeContext.ts
@@ -5,12 +5,10 @@ import {Theme} from '../../models';
 
 export interface ThemeContextProps {
     theme: Theme;
-    setTheme: (newTheme: Theme) => void;
 }
 
 export const initialValue: ThemeContextProps = {
     theme: DEFAULT_THEME,
-    setTheme: () => {},
 };
 
 export const ThemeContext = React.createContext(initialValue);
