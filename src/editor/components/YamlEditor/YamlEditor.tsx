95a7f7b24be1cde37cd5f3cce42187c98fc62726
kkmch <kkmch@yandex-team.ru>
2023-07-24 10:42:32 +0300

    feat: added yaml editor

---
+++ src/editor/components/YamlEditor/YamlEditor.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import yaml from 'js-yaml';
+import MonacoEditor from 'react-monaco-editor';
+
+import {PageContent} from '../../../models';
+
+interface YamlEditorProps {
+    content: PageContent;
+}
+
+export const YamlEditor = ({content}: YamlEditorProps) => {
+    return (
+        <MonacoEditor
+            value={yaml.dump(content)}
+            language="yaml"
+            options={{
+                minimap: {
+                    enabled: false,
+                },
+                renderWhitespace: 'all',
+                overviewRulerLanes: 0,
+                hideCursorInOverviewRuler: true,
+                scrollbar: {
+                    vertical: 'hidden',
+                },
+                overviewRulerBorder: false,
+                readOnly: true,
+                language: 'yaml',
+            }}
+        />
+    );
+};
