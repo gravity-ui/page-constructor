db2334c81e0ca4f6db09cbaf573ce5e13d503b1a
kkmch <kkmch@yandex-team.ru>
2023-08-11 09:03:40 +0300

    feat: tests for image and video

--- src/components/Media/__tests__/Media.test.tsx
+++ src/components/Media/__tests__/Media.test.tsx
@@ -5,7 +5,8 @@ import {render, screen} from '@testing-library/react';
 import {MediaVideoControlsType} from '../../../models';
 import Media, {MediaAllProps} from '../Media';
 
-const testingLabel = 'Example of media description';
+const videoTestingLabel = 'Example of video description';
+const imageTestingLabel = 'Example of image description';
 const videoSrc = [
     'https://storage.yandexcloud.net/cloud-www-assets/constructor/storybook/images/video_8-12_white.webm',
     'https://storage.yandexcloud.net/cloud-www-assets/constructor/storybook/images/video_8-12_white.mp4',
@@ -16,23 +17,71 @@ const imageSrc =
 
 describe('Media', () => {
     test.each<MediaAllProps>([
-        {video: {src: videoSrc, ariaLabel: testingLabel, controls: MediaVideoControlsType.Default}},
-        {video: {src: videoSrc, ariaLabel: testingLabel, controls: MediaVideoControlsType.Custom}},
-        {image: {src: imageSrc, alt: testingLabel}},
-    ])('video should have aria-label', (props) => {
+        {
+            video: {
+                src: videoSrc,
+                ariaLabel: videoTestingLabel,
+                controls: MediaVideoControlsType.Default,
+            },
+        },
+        {
+            video: {
+                src: videoSrc,
+                ariaLabel: videoTestingLabel,
+                controls: MediaVideoControlsType.Custom,
+            },
+        },
+    ])('video should have customizable aria-label', (props) => {
         render(<Media playVideo={false} {...props} />);
-        const element = screen.getByLabelText(testingLabel);
+        const element = screen.getByLabelText(videoTestingLabel, {selector: 'video'});
         expect(element).toBeTruthy();
     });
 
     test.each<MediaAllProps>([
-        {image: {src: imageSrc, alt: testingLabel}},
-        {image: {src: imageSrc, desktop: imageSrc, alt: testingLabel}},
-        {image: {src: imageSrc, mobile: imageSrc, alt: testingLabel}},
-        {image: {src: imageSrc, mobile: imageSrc, alt: testingLabel, disableCompress: true}},
+        {video: {src: videoSrc, controls: MediaVideoControlsType.Custom}},
+        {video: {src: videoSrc, controls: MediaVideoControlsType.Default}},
+    ])('video should not have aria-label if not set', (props) => {
+        render(<Media playVideo={false} {...props} />);
+        const element = screen.queryByLabelText(imageTestingLabel, {selector: 'video'});
+        expect(element).toBeFalsy();
+    });
+
+    test.each<MediaAllProps>([
+        {image: {src: imageSrc, alt: imageTestingLabel}},
+        {image: {src: imageSrc, desktop: imageSrc, alt: imageTestingLabel}},
+        {image: {src: imageSrc, mobile: imageSrc, alt: imageTestingLabel}},
+        {image: {src: imageSrc, mobile: imageSrc, alt: imageTestingLabel, disableCompress: true}},
     ])('image should have customizable alt props', (props) => {
         render(<Media playVideo={false} {...props} />);
-        const element = screen.getByAltText(testingLabel);
+        const element = screen.getByAltText(imageTestingLabel);
         expect(element).toBeTruthy();
+    });
+
+    test.each<MediaAllProps>([
+        {image: {src: imageSrc}},
+        {image: {src: imageSrc, desktop: imageSrc}},
+        {image: {src: imageSrc, mobile: imageSrc}},
+        {image: {src: imageSrc, mobile: imageSrc, disableCompress: true}},
+    ])('image should not have alt attribute if not set', (props) => {
+        render(<Media playVideo={false} {...props} />);
+        const element = screen.queryByAltText(imageTestingLabel);
+        expect(element).toBeFalsy();
+    });
+
+    test.each<MediaAllProps>([
+        {
+            image: {src: imageSrc, alt: imageTestingLabel},
+            video: {
+                src: videoSrc,
+                ariaLabel: videoTestingLabel,
+                controls: MediaVideoControlsType.Default,
+            },
+        },
+    ])('should have both custom alt and aria-label', (props) => {
+        render(<Media playVideo={false} {...props} />);
+        const imageElement = screen.getByAltText(imageTestingLabel);
+        const videoElement = screen.getByLabelText(videoTestingLabel);
+        expect(imageElement).toBeTruthy();
+        expect(videoElement).toBeTruthy();
     });
 });
