c9a5e9fbda768699cc78745bf2fd1a02f3d9735e
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2023-08-14 17:29:12 +0300

    fix(Icons): make icon without href no clickable (#500)

--- src/blocks/Icons/Icons.tsx
+++ src/blocks/Icons/Icons.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {Fragment, useContext} from 'react';
 
 import {Image, Title} from '../../components';
 import {LocationContext} from '../../context/locationContext';
@@ -9,23 +9,36 @@ import './Icons.scss';
 
 const b = block('icons-block');
 
+const getItemContent = (item: IconsBlockProps['items'][number]) => (
+    <Fragment>
+        <Image className={b('image')} src={item.src} />
+        <p className={b('text')}>{item.text}</p>
+    </Fragment>
+);
+
 const Icons = ({title, size = 's', items}: IconsBlockProps) => {
     const {hostname} = useContext(LocationContext);
 
     return (
         <div className={b({size})}>
             {title && <Title className={b('header')} title={title} colSizes={{all: 12}} />}
-            {items.map((item) => (
-                <a
-                    className={b('item')}
-                    key={item.url}
-                    href={item.url}
-                    {...getLinkProps(item.url, hostname)}
-                >
-                    <Image className={b('image')} src={item.src} />
-                    <p className={b('text')}>{item.text}</p>
-                </a>
-            ))}
+            {items.map((item) => {
+                const itemContent = getItemContent(item);
+                return item.url ? (
+                    <a
+                        className={b('item')}
+                        key={item.url}
+                        href={item.url}
+                        {...getLinkProps(item.url, hostname)}
+                    >
+                        {itemContent}
+                    </a>
+                ) : (
+                    <div className={b('item')} key={item.url}>
+                        {itemContent}
+                    </div>
+                );
+            })}
         </div>
     );
 };
