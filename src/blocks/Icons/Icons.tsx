ebb965064e88dabf4f94128a92a3c1856fbd0a2b
aeksandla <aeksandla@yandex-team.ru>
2023-08-14 17:05:51 +0300

    fix(Icons): fix

--- src/blocks/Icons/Icons.tsx
+++ src/blocks/Icons/Icons.tsx
@@ -8,35 +8,37 @@ import {block, getLinkProps} from '../../utils';
 import './Icons.scss';
 
 const b = block('icons-block');
+
+const getItemContent = (item: IconsBlockProps['items'][number]) => (
+    <Fragment>
+        <Image className={b('image')} src={item.src} />
+        <p className={b('text')}>{item.text}</p>
+    </Fragment>
+);
+
 const Icons = ({title, size = 's', items}: IconsBlockProps) => {
     const {hostname} = useContext(LocationContext);
-
-    const itemContent = (item: IconsBlockProps['items'][number]) => (
-        <Fragment>
-            <Image className={b('image')} src={item.src} />
-            <p className={b('text')}>{item.text}</p>
-        </Fragment>
-    );
 
     return (
         <div className={b({size})}>
             {title && <Title className={b('header')} title={title} colSizes={{all: 12}} />}
-            {items.map((item) =>
-                item.url ? (
+            {items.map((item) => {
+                const itemContent = getItemContent(item);
+                return item.url ? (
                     <a
                         className={b('item')}
                         key={item.url}
                         href={item.url}
                         {...getLinkProps(item.url, hostname)}
                     >
-                        {itemContent(item)}
+                        {itemContent}
                     </a>
                 ) : (
                     <div className={b('item')} key={item.url}>
-                        {itemContent(item)}
+                        {itemContent}
                     </div>
-                ),
-            )}
+                );
+            })}
         </div>
     );
 };
