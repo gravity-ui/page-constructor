ffaaca9bd4e06d00a8fb07b677b7e80ed284454b
kkmch <kkmch@yandex-team.ru>
2023-08-21 15:21:30 +0300

    fix: more QuestionBlockItem to folder

---
+++ src/blocks/Questions/QuestionBlockItem/QuestionBlockItem.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import {useActionHandlers} from '@gravity-ui/uikit';
+
+import {Foldable, HTML, ToggleArrow, YFMWrapper} from '../../../components';
+import Link from '../../../components/Link/Link';
+import {QuestionBlockItemProps} from '../../../models';
+import {block} from '../../../utils';
+import {FaqMicrodataValues} from '../models';
+
+import './QuestionBlockItem.scss';
+
+const b = block('QuestionsBlockItem');
+
+export const QuestionBlockItem = ({
+    title: itemTitle,
+    text: itemText,
+    link,
+    listStyle = 'dash',
+    isOpened,
+    onClick,
+}: QuestionBlockItemProps) => {
+    const {onKeyDown} = useActionHandlers(onClick);
+
+    return (
+        <div
+            className={b()}
+            itemScope
+            itemProp={FaqMicrodataValues.QuestionProp}
+            itemType={FaqMicrodataValues.QuestionType}
+            role={'listitem'}
+        >
+            <h3
+                className={b('title')}
+                onClick={onClick}
+                aria-expanded={isOpened}
+                role={'button'}
+                tabIndex={0}
+                onKeyDown={onKeyDown}
+            >
+                <HTML itemProp={FaqMicrodataValues.QuestionNameProp}>{itemTitle}</HTML>
+                <ToggleArrow
+                    open={isOpened}
+                    size={16}
+                    type={'vertical'}
+                    iconType="navigation"
+                    className={b('arrow')}
+                />
+            </h3>
+            <Foldable isOpened={isOpened}>
+                <div
+                    className={b('text')}
+                    itemScope
+                    itemProp={FaqMicrodataValues.AnswerProp}
+                    itemType={FaqMicrodataValues.AnswerType}
+                    aria-hidden={!isOpened}
+                >
+                    <YFMWrapper
+                        content={itemText}
+                        modifiers={{
+                            constructor: true,
+                            constructorListStyle: true,
+                            constructorListStyleDash: listStyle === 'dash',
+                        }}
+                        itemProp={FaqMicrodataValues.QuestionTextProp}
+                    />
+                    {link && <Link {...link} tabIndex={isOpened ? 0 : -1} className={b('link')} />}
+                </div>
+            </Foldable>
+        </div>
+    );
+};
--- src/blocks/Questions/Questions.tsx
+++ src/blocks/Questions/Questions.tsx
@@ -1,87 +1,16 @@
 import React, {useState} from 'react';
 
-import {useActionHandlers} from '@gravity-ui/uikit';
-
-import {Foldable, HTML, ToggleArrow, YFMWrapper} from '../../components';
-import Link from '../../components/Link/Link';
 import {Col, Row} from '../../grid';
-import {QuestionBlockItemProps, QuestionsProps} from '../../models';
+import {QuestionsProps} from '../../models';
 import {Content} from '../../sub-blocks';
 import {block} from '../../utils';
+
+import {QuestionBlockItem} from './QuestionBlockItem/QuestionBlockItem';
+import {FaqMicrodataValues} from './models';
 
 import './Questions.scss';
 
 const b = block('QuestionsBlock');
-
-const FaqMicrodataValues = {
-    PageType: 'https://schema.org/FAQPage',
-    QuestionType: 'https://schema.org/Question',
-    QuestionProp: 'mainEntity',
-    QuestionNameProp: 'name',
-    QuestionTextProp: 'text',
-    AnswerType: 'https://schema.org/Answer',
-    AnswerProp: 'acceptedAnswer',
-    AnswerTextProp: 'text',
-} as const;
-
-const QuestionBlockItem = ({
-    title: itemTitle,
-    text: itemText,
-    link,
-    listStyle = 'dash',
-    isOpened,
-    onClick,
-}: QuestionBlockItemProps) => {
-    const {onKeyDown} = useActionHandlers(onClick);
-
-    return (
-        <div
-            className={b('item')}
-            itemScope
-            itemProp={FaqMicrodataValues.QuestionProp}
-            itemType={FaqMicrodataValues.QuestionType}
-            role={'listitem'}
-        >
-            <h3
-                className={b('item-title')}
-                onClick={onClick}
-                aria-expanded={isOpened}
-                role={'button'}
-                tabIndex={0}
-                onKeyDown={onKeyDown}
-            >
-                <HTML itemProp={FaqMicrodataValues.QuestionNameProp}>{itemTitle}</HTML>
-                <ToggleArrow
-                    open={isOpened}
-                    size={16}
-                    type={'vertical'}
-                    iconType="navigation"
-                    className={b('arrow')}
-                />
-            </h3>
-            <Foldable isOpened={isOpened}>
-                <div
-                    className={b('text')}
-                    itemScope
-                    itemProp={FaqMicrodataValues.AnswerProp}
-                    itemType={FaqMicrodataValues.AnswerType}
-                    aria-hidden={!isOpened}
-                >
-                    <YFMWrapper
-                        content={itemText}
-                        modifiers={{
-                            constructor: true,
-                            constructorListStyle: true,
-                            constructorListStyleDash: listStyle === 'dash',
-                        }}
-                        itemProp={FaqMicrodataValues.QuestionTextProp}
-                    />
-                    {link && <Link {...link} tabIndex={isOpened ? 0 : -1} className={b('link')} />}
-                </div>
-            </Foldable>
-        </div>
-    );
-};
 
 const QuestionsBlock = (props: QuestionsProps) => {
     const {title, text, additionalInfo, links, buttons, items} = props;
@@ -91,7 +20,7 @@ const QuestionsBlock = (props: QuestionsProps) => {
         let newState;
 
         if (opened.includes(index)) {
-            newState = opened.filter((intemIndex: number) => intemIndex !== index);
+            newState = opened.filter((itemIndex: number) => itemIndex !== index);
         } else {
             newState = [...opened, index];
         }
