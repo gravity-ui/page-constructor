5dd578d95e088a45b1c2c2b18c089702698fe897
kkmch <kkmch@yandex-team.ru>
2023-10-27 10:59:31 +0200

    feat: added new story for quote types

--- src/sub-blocks/Quote/__stories__/Quote.stories.tsx
+++ src/sub-blocks/Quote/__stories__/Quote.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Meta, StoryFn} from '@storybook/react';
 
-import {QuoteProps} from '../../../models';
+import {QuoteProps, QuoteType} from '../../../models';
 import Quote from '../Quote';
 
 import data from './data.json';
@@ -22,11 +22,19 @@ const DefaultTemplate: StoryFn<QuoteProps> = (args) => (
         <Quote {...args} />
     </div>
 );
+const QuoteTypesTemplate: StoryFn<QuoteProps> = (args) => (
+    <div style={{maxWidth: '1248px', display: 'flex', flexDirection: 'column', gap: '24px'}}>
+        <Quote {...args} quoteType={QuoteType.Chevron} />
+        <Quote {...args} quoteType={QuoteType.EnglishDouble} />
+    </div>
+);
 export const Default = DefaultTemplate.bind({});
+export const QuoteTypes = QuoteTypesTemplate.bind({});
 export const BorderLine = DefaultTemplate.bind({});
 export const DarkTheme = DefaultTemplate.bind({});
 
 Default.args = data.default.content as QuoteProps;
+QuoteTypes.args = data.default.content as QuoteProps;
 BorderLine.args = {
     ...data.default.content,
     ...data.borderLine.content,
