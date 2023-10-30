8713eb0a8d7b8646389bb5b9431847a54ef9695d
3y3 <3y3@ya.ru>
2023-10-30 14:57:16 +0300

    fix: fix logo router link rendering

--- src/navigation/components/Logo/Logo.tsx
+++ src/navigation/components/Logo/Logo.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Fragment, useContext} from 'react';
 
 import {Image} from '../../../components';
 import {getMediaImage} from '../../../components/Media/Image/utils';
 import RouterLink from '../../../components/RouterLink/RouterLink';
+import {LocationContext} from '../../../context/locationContext';
 import {useTheme} from '../../../context/theme';
 import {ThemedNavigationLogoData} from '../../../models';
-import {block, getThemedValue} from '../../../utils';
+import {block, getLinkProps, getThemedValue} from '../../../utils';
 
 import './Logo.scss';
 
@@ -16,17 +17,35 @@ export type LogoProps = ThemedNavigationLogoData & {
 };
 
 const Logo: React.FC<LogoProps> = (props) => {
+    const {hostname, Link} = useContext(LocationContext);
     const theme = useTheme();
     const themedLogoProps = getThemedValue(props, theme) || props;
     const imageData = getMediaImage(themedLogoProps.icon || props.icon);
     const textData = themedLogoProps.text || props.text;
+    const url = themedLogoProps.url || props.url || '/';
+    const urlTitle = themedLogoProps.urlTitle || props.urlTitle || textData;
+    const linkExtraProps = getLinkProps(url, hostname);
+    const content = (
+        <Fragment>
+            {imageData && <Image className={b('icon')} {...imageData} />}
+            <span className={b('text')}>{textData}</span>
+        </Fragment>
+    );
 
     return (
-        <RouterLink href={themedLogoProps.url || props.url || '/'} passHref>
-            <div className={b(null, props.className)}>
-                {imageData && <Image className={b('icon')} {...imageData} />}
-                <span className={b('text')}>{textData}</span>
-            </div>
+        <RouterLink href={url} passHref>
+            {Link ? (
+                <span className={b(null, props.className)}>{content}</span>
+            ) : (
+                <a
+                    className={b(null, props.className)}
+                    href={url}
+                    title={urlTitle}
+                    {...linkExtraProps}
+                >
+                    {content}
+                </a>
+            )}
         </RouterLink>
     );
 };
