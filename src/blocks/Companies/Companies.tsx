76591a82a6f79ca04e57f216b54fe5c9c7218708
VitaliiDC8 <vitalii8@double.cloud>
2023-11-14 15:45:37 +0300

    feat: added description field to companies block

--- src/blocks/Companies/Companies.tsx
+++ src/blocks/Companies/Companies.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Image} from '../../components';
+import {Image, YFMWrapper} from '../../components';
 import AnimateBlock from '../../components/AnimateBlock/AnimateBlock';
 import {useTheme} from '../../context/theme';
 import {CompaniesBlockProps} from '../../models';
@@ -10,7 +10,7 @@ import './Companies.scss';
 
 const b = block('companies-block');
 
-export const CompaniesBlock = ({title, images, animated}: CompaniesBlockProps) => {
+export const CompaniesBlock = ({title, description, images, animated}: CompaniesBlockProps) => {
     const theme = useTheme();
     const themedImages = getThemedValue(images, theme) || {};
 
@@ -18,6 +18,11 @@ export const CompaniesBlock = ({title, images, animated}: CompaniesBlockProps) =
         <AnimateBlock className={b()} offset={150} animate={animated}>
             <div className={b('content')}>
                 <h2 className={b('title')}>{title}</h2>
+                {description && (
+                    <div className={b('description')}>
+                        <YFMWrapper content={description} modifiers={{constructor: true}} />
+                    </div>
+                )}
                 <div className={b('images')}>
                     <Image {...themedImages} className={b('image')} />
                 </div>
