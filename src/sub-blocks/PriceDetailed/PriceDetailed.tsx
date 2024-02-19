36516561ef9ec7483971e84b39517b254bafe51a
Sergey <148218517+qradle-yndx@users.noreply.github.com>
2024-02-19 12:51:22 +0300

    fix: price detailed deprecated jsdoc linter warning (#821)

--- src/sub-blocks/PriceDetailed/PriceDetailed.tsx
+++ src/sub-blocks/PriceDetailed/PriceDetailed.tsx
@@ -13,6 +13,7 @@ import PriceDescription from './PriceDescription/PriceDescription';
 import PriceDetails from './PriceDetails/PriceDetails';
 import SeparatePriceDetailed from './SeparatePriceDetailed/SeparatePriceDetailed';
 
+// eslint-disable-next-line valid-jsdoc
 /** @deprecated */
 const PriceDetailed = (props: PriceDetailedProps) => {
     const {
