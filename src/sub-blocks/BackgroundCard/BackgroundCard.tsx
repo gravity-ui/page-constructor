bce23b347623fa4a1503f75b288e31bc9f56a62a
Roman Karpov <roman-karpov@double.cloud>
2024-03-04 15:00:42 +0100

    fix(Content): revert renderСontentControls adding (#840)
    * fix(Content): revert renderСontentControls adding
    
    * fix(LayoutItem): simplify controls layout

--- src/sub-blocks/BackgroundCard/BackgroundCard.tsx
+++ src/sub-blocks/BackgroundCard/BackgroundCard.tsx
@@ -1,14 +1,12 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 
 import {useUniqId} from '@gravity-ui/uikit';
 
-import {BackgroundImage, CardBase} from '../../components/';
+import {BackgroundImage, Buttons, CardBase, Links} from '../../components/';
 import {useTheme} from '../../context/theme';
 import {BackgroundCardProps} from '../../models';
 import {block, getThemedValue} from '../../utils';
-import renderContentControls from '../../utils/renderContentControls/renderContentControls';
 import Content from '../Content/Content';
-import renderCardFooterControlsContainer from '../renderCardFooterControlsContainer/renderCardFooterControlsContainer';
 
 import './BackgroundCard.scss';
 
@@ -39,20 +37,6 @@ const BackgroundCard = (props: BackgroundCardProps) => {
     const borderType = hasBackgroundColor ? 'none' : border;
     const areControlsInFooter = !paddingBottom && controlPosition === 'footer';
 
-    const footerControls = useMemo(
-        () =>
-            renderContentControls(
-                {
-                    links: areControlsInFooter ? links : undefined,
-                    buttons: areControlsInFooter ? buttons : undefined,
-                    size: 's',
-                    titleId,
-                },
-                renderCardFooterControlsContainer,
-            ),
-        [areControlsInFooter, links, buttons, titleId],
-    );
-
     return (
         <CardBase
             className={b({padding: paddingBottom, theme: cardTheme})}
@@ -79,7 +63,17 @@ const BackgroundCard = (props: BackgroundCardProps) => {
                     colSizes={{all: 12, md: 12}}
                 />
             </CardBase.Content>
-            {footerControls}
+            {areControlsInFooter && (links || buttons) && (
+                <CardBase.Footer>
+                    <Links className={b('links')} size="s" links={links} titleId={titleId} />
+                    <Buttons
+                        className={b('buttons')}
+                        size="s"
+                        buttons={buttons}
+                        titleId={titleId}
+                    />
+                </CardBase.Footer>
+            )}
         </CardBase>
     );
 };
