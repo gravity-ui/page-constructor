ecf6df2e813b7122884541db731cdf7d04da7df2
NasgulNexus <mostovoyegor@yandex-team.ru>
2024-03-11 19:38:17 +0100

    test(Playwright): add before mount theme and mobile provider

--- playwright/playwright/index.tsx
+++ playwright/playwright/index.tsx
@@ -1,2 +1,15 @@
-// Import styles, initialize component theme here.
+import {MobileProvider, ThemeProvider} from '@gravity-ui/uikit';
+import {beforeMount} from '@playwright/experimental-ct-react/hooks';
+import React from 'react';
+
 import './index.scss';
+
+beforeMount(async ({App}) => {
+    return (
+        <ThemeProvider theme="light">
+            <MobileProvider>
+                <App />
+            </MobileProvider>
+        </ThemeProvider>
+    );
+});
