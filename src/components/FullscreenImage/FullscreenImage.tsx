18a701dc85af9a13bbead0a099b537f4e9d4f8e1
qradle <qradle@yandex-team.ru>
2024-04-02 00:05:15 +0300

    feat: add media images fullscreen slider

--- src/components/FullscreenImage/FullscreenImage.tsx
+++ src/components/FullscreenImage/FullscreenImage.tsx
@@ -3,8 +3,11 @@ import React, {CSSProperties, HTMLProps, useState} from 'react';
 import {ChevronsExpandUpRight, Xmark} from '@gravity-ui/icons';
 import {Icon, Modal} from '@gravity-ui/uikit';
 
+import SliderBlock from '../../blocks/Slider/Slider';
+import {ImageProps as ModelImageProps, SliderType} from '../../models';
 import {block} from '../../utils';
 import Image, {ImageProps} from '../Image/Image';
+import {getMediaImage} from '../Media/Image/utils';
 
 import {i18n} from './i18n';
 
@@ -15,6 +18,7 @@ export interface FullscreenImageProps extends ImageProps {
     modalImageClass?: string;
     imageStyle?: CSSProperties;
     extraProps?: HTMLProps<HTMLDivElement>;
+    sliderData?: {items: ModelImageProps[]; index: number};
 }
 
 const b = block('fullscreen-image');
@@ -22,7 +26,14 @@ const FULL_SCREEN_ICON_SIZE = 18;
 const CLOSE_ICON_SIZE = 24;
 
 const FullscreenImage = (props: FullscreenImageProps) => {
-    const {imageClassName, modalImageClass, imageStyle, alt = i18n('img-alt'), extraProps} = props;
+    const {
+        imageClassName,
+        sliderData,
+        modalImageClass,
+        imageStyle,
+        alt = i18n('img-alt'),
+        extraProps,
+    } = props;
     const [isOpened, setIsOpened] = useState(false);
 
     const openModal = () => setIsOpened(true);
@@ -66,7 +77,29 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                             className={b('icon', {hover: true})}
                         />
                     </button>
-                    <Image {...props} className={b('modal-image', modalImageClass)} />
+                    {!sliderData ? (
+                        <Image {...props} className={b('modal-image', modalImageClass)} />
+                    ) : (
+                        <div className={b('modal-slider')}>
+                            <SliderBlock
+                                initialIndex={sliderData.index}
+                                slidesToShow={1}
+                                type={SliderType.MediaCard}
+                            >
+                                {sliderData.items.map((item, index) => {
+                                    const itemData = getMediaImage(item);
+                                    return (
+                                        <div key={index} className={b('modal-slider_item')}>
+                                            <Image
+                                                className={b('modal-slider_item-image')}
+                                                {...itemData}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </SliderBlock>
+                        </div>
+                    )}
                 </Modal>
             )}
         </div>
