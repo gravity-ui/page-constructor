dc9611fb343788390609bb1a40831bf9ebe0acb4
qradle <qradle@yandex-team.ru>
2024-04-11 15:13:37 +0300

    feat: add media images fullscreen slider

--- src/components/FullscreenImage/FullscreenImage.tsx
+++ src/components/FullscreenImage/FullscreenImage.tsx
@@ -1,10 +1,13 @@
-import React, {CSSProperties, HTMLProps, useState} from 'react';
+import React, {CSSProperties, HTMLProps, useEffect, useState} from 'react';
 
 import {ChevronsExpandUpRight, Xmark} from '@gravity-ui/icons';
 import {Icon, Modal} from '@gravity-ui/uikit';
 
+import SliderBlock from '../../blocks/Slider/Slider';
+import {ImageProps as ModelImageProps, SliderType} from '../../models';
 import {block} from '../../utils';
 import Image, {ImageProps} from '../Image/Image';
+import {getMediaImage} from '../Media/Image/utils';
 
 import {i18n} from './i18n';
 
@@ -15,6 +18,7 @@ export interface FullscreenImageProps extends ImageProps {
     modalImageClass?: string;
     imageStyle?: CSSProperties;
     extraProps?: HTMLProps<HTMLDivElement>;
+    sliderData?: {items: ModelImageProps[]; initialIndex: number};
 }
 
 const b = block('fullscreen-image');
@@ -22,11 +26,29 @@ const FULL_SCREEN_ICON_SIZE = 18;
 const CLOSE_ICON_SIZE = 24;
 
 const FullscreenImage = (props: FullscreenImageProps) => {
-    const {imageClassName, modalImageClass, imageStyle, alt = i18n('img-alt'), extraProps} = props;
+    const {
+        imageClassName,
+        sliderData,
+        modalImageClass,
+        imageStyle,
+        alt = i18n('img-alt'),
+        extraProps,
+    } = props;
     const [isOpened, setIsOpened] = useState(false);
+    const [sliderLoaded, setSliderLoaded] = useState(false);
 
     const openModal = () => setIsOpened(true);
     const closeModal = () => setIsOpened(false);
+
+    useEffect(() => {
+        if (!isOpened) {
+            setSliderLoaded(false);
+        }
+    }, [isOpened]);
+
+    const handleSliderImageLoad = () => {
+        setSliderLoaded(true);
+    };
 
     return (
         <div className={b()} {...extraProps}>
@@ -51,8 +73,8 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                 <Modal
                     open={isOpened}
                     onClose={closeModal}
-                    className={b('modal')}
-                    contentClassName={b('modal-content')}
+                    className={b('modal', {slider: !!sliderData})}
+                    contentClassName={b('modal-content', {loaded: sliderLoaded})}
                 >
                     <button
                         className={b('icon-wrapper', {visible: true})}
@@ -66,7 +88,30 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                             className={b('icon', {hover: true})}
                         />
                     </button>
-                    <Image {...props} className={b('modal-image', modalImageClass)} />
+                    {sliderData ? (
+                        <div className={b('modal-slider')}>
+                            <SliderBlock
+                                initialIndex={sliderData.initialIndex}
+                                slidesToShow={1}
+                                type={SliderType.MediaCard}
+                            >
+                                {sliderData.items.map((item, index) => (
+                                    <div key={index} className={b('modal-slider_item')}>
+                                        <Image
+                                            onLoad={handleSliderImageLoad}
+                                            className={b(
+                                                'modal-slider_item-image',
+                                                modalImageClass,
+                                            )}
+                                            {...getMediaImage(item)}
+                                        />
+                                    </div>
+                                ))}
+                            </SliderBlock>
+                        </div>
+                    ) : (
+                        <Image {...props} className={b('modal-image', modalImageClass)} />
+                    )}
                 </Modal>
             )}
         </div>
