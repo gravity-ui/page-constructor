487b7a7f81cf0224e0dfb90bf3b8e1a32d0f3698
aeksandla <aeksandla@yandex-team.ru>
2024-05-03 15:50:41 +0300

    feat: fix MediaCard

--- src/sub-blocks/MediaCard/MediaCard.tsx
+++ src/sub-blocks/MediaCard/MediaCard.tsx
@@ -1,30 +1,24 @@
 import React from 'react';
 
 import {CardBase, Media} from '../../components';
-import {useTheme} from '../../context/theme';
-import {MediaCardProps, MediaProps} from '../../models';
-import {block, getThemedValue} from '../../utils';
+import {MediaCardProps} from '../../models';
+import {block} from '../../utils';
 
 import './MediaCard.scss';
 
 const b = block('MediaCard');
 
-const MediaCard = ({border, analyticsEvents, ...mediaProps}: MediaCardProps) => {
-    const theme = useTheme();
-    const themedMedia = getThemedValue(mediaProps as MediaProps, theme);
-
-    return (
-        <CardBase
-            className={b()}
-            bodyClassName={b('body')}
-            border={border}
-            analyticsEvents={analyticsEvents}
-        >
-            <CardBase.Content>
-                <Media {...themedMedia} />
-            </CardBase.Content>
-        </CardBase>
-    );
-};
+const MediaCard = ({border, analyticsEvents, ...mediaProps}: MediaCardProps) => (
+    <CardBase
+        className={b()}
+        bodyClassName={b('body')}
+        border={border}
+        analyticsEvents={analyticsEvents}
+    >
+        <CardBase.Content>
+            <Media {...mediaProps} />
+        </CardBase.Content>
+    </CardBase>
+);
 
 export default MediaCard;
