0e4dfd893e90c5b637b38ac0601a9fd7d505a008
qradle <qradle@yandex-team.ru>
2024-07-29 14:41:46 +0300

    feat: add media images fullscreen new slider

--- src/components/FullscreenImage/FullscreenImage.tsx
+++ src/components/FullscreenImage/FullscreenImage.tsx
@@ -3,7 +3,7 @@ import React, {CSSProperties, HTMLProps, useEffect, useState} from 'react';
 import {ChevronsExpandUpRight, Xmark} from '@gravity-ui/icons';
 import {Icon, Modal} from '@gravity-ui/uikit';
 
-import SliderBlock from '../../blocks/Slider/Slider';
+import {SliderNewBlock as SliderBlock} from '../../blocks/unstable';
 import {ImageProps as ModelImageProps, SliderType} from '../../models';
 import {block} from '../../utils';
 import Image, {ImageProps} from '../Image/Image';
@@ -73,7 +73,7 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                 <Modal
                     open={isOpened}
                     onClose={closeModal}
-                    className={b('modal', {slider: Boolean(sliderData)})}
+                    className={b('modal', {'with-slider': Boolean(sliderData)})}
                     contentClassName={b('modal-content', {loaded: sliderLoaded})}
                 >
                     <button
@@ -91,9 +91,9 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                     {sliderData ? (
                         <div className={b('modal-slider')}>
                             <SliderBlock
-                                initialIndex={sliderData.initialIndex}
+                                initialSlide={sliderData.initialIndex}
                                 slidesToShow={1}
-                                type={SliderType.MediaCard}
+                                type={SliderType.FullscreenCard}
                             >
                                 {sliderData.items.map((item, index) => (
                                     <div key={index} className={b('modal-slider_item')}>
@@ -102,6 +102,9 @@ const FullscreenImage = (props: FullscreenImageProps) => {
                                             className={b(
                                                 'modal-slider_item-image',
                                                 modalImageClass,
+                                            )}
+                                            containerClassName={b(
+                                                'modal-slider_item-image-wrapper',
                                             )}
                                             {...getMediaImage(item)}
                                         />
