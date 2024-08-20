4b49f9b408086eed51539426ca97e086dbe60ea0
Ruslan Bagautdinov <bagautdinovrl@yandex-team.ru>
2024-08-20 14:51:03 +0300

    revert: change size in MobileMenuButton

--- src/navigation/components/MobileMenuButton/MobileMenuButton.tsx
+++ src/navigation/components/MobileMenuButton/MobileMenuButton.tsx
@@ -24,7 +24,7 @@ export const MobileMenuButton: React.FC<MobileMenuButtonProps> = ({
 
             onSidebarOpenedChange(!isSidebarOpened);
         }}
-        size={'l'}
+        size="l"
         icon={isSidebarOpened ? Xmark : Bars}
         iconSize={ICON_SIZE}
     />
