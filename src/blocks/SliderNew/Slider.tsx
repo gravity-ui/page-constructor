6a7c1ede6371b7883df42d5f319ecef9cf20c047
qradle <qradle@yandex-team.ru>
2024-08-30 21:43:39 +0300

    fix: slider arrows merge

--- src/blocks/SliderNew/Slider.tsx
+++ src/blocks/SliderNew/Slider.tsx
@@ -6,7 +6,7 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import Anchor from '../../components/Anchor/Anchor';
 import AnimateBlock from '../../components/AnimateBlock/AnimateBlock';
 import Title from '../../components/Title/Title';
-import {ClassNameProps, Refable, SliderProps as SliderParams} from '../../models';
+import {ClassNameProps, Refable, SliderProps as SliderParams, SliderType} from '../../models';
 import {block} from '../../utils';
 
 import Arrow from './Arrow/Arrow';
@@ -144,6 +144,7 @@ export const SliderNewBlock = ({
                             <Arrow
                                 className={b('arrow', {prev: true})}
                                 type="left"
+                                transparent={type === SliderType.HeaderCard}
                                 onClick={onPrev}
                                 size={arrowSize}
                                 extraProps={{tabIndex: controlTabIndex}}
@@ -151,6 +152,7 @@ export const SliderNewBlock = ({
                             <Arrow
                                 className={b('arrow', {next: true})}
                                 type="right"
+                                transparent={type === SliderType.HeaderCard}
                                 onClick={onNext}
                                 size={arrowSize}
                                 extraProps={{tabIndex: controlTabIndex}}
