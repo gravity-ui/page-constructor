93e75e9ce68e957eae98bd3b1fa45550800dd956
Nik Tverd <61203447+niktverd@users.noreply.github.com>
2024-09-25 18:17:07 +0500

    test: skip Map test (#1030)

--- src/blocks/Map/__tests__/Map.visual.test.tsx
+++ src/blocks/Map/__tests__/Map.visual.test.tsx
@@ -7,25 +7,25 @@ import {Default, Direction, MapsTypes, Size} from './helpers';
 const DEFAULT_MAP_DELAY = 20 * 1000;
 
 test.describe('Map', () => {
-    test('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
+    test.skip('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
         await mount(<Default />);
         await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark'});
     });
 
-    test('render stories <Size>', async ({mount, expectScreenshot, delay}) => {
+    test.skip('render stories <Size>', async ({mount, expectScreenshot, delay}) => {
         await mount(<Size />);
         await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark'});
     });
 
-    test('render stories <Direction>', async ({mount, expectScreenshot, delay}) => {
+    test.skip('render stories <Direction>', async ({mount, expectScreenshot, delay}) => {
         await mount(<Direction />);
         await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark'});
     });
 
-    test('render stories <MapsTypes>', async ({mount, expectScreenshot, delay}) => {
+    test.skip('render stories <MapsTypes>', async ({mount, expectScreenshot, delay}) => {
         await mount(<MapsTypes />);
         await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark'});
