218f1c49b68f6c9b9d576d0f405347a71913dd6f
Egor Mostovoy <mostovoyegor@yandex-team.ru>
2024-09-26 10:27:34 +0200

    test: playwright cache (#1029)
    * test: playwright cache
    
    * fix: add mask test map
    
    * fix: lint

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -18,7 +18,10 @@ reporter.push(
         'html',
         {
             open: process.env.CI ? 'never' : 'on-failure',
-            outputFolder: resolve(process.cwd(), 'playwright-report'),
+            outputFolder: resolve(
+                process.cwd(),
+                process.env.IS_DOCKER ? 'playwright-report-docker' : 'playwright-report',
+            ),
         },
     ],
 );
@@ -54,6 +57,7 @@ const config: PlaywrightTestConfig = {
         /* Port to use for Playwright component endpoint. */
         screenshot: 'only-on-failure',
         timezoneId: 'UTC',
+        ctCacheDir: process.env.IS_DOCKER ? '.cache-docker' : '.cache',
         ctViteConfig: {
             publicDir: pathFromRoot('.storybook/public'),
             json: {
--- src/blocks/Map/__tests__/Map.visual.test.tsx
+++ src/blocks/Map/__tests__/Map.visual.test.tsx
@@ -4,30 +4,28 @@ import {test} from '../../../../playwright/core/index';
 
 import {Default, Direction, MapsTypes, Size} from './helpers';
 
-const DEFAULT_MAP_DELAY = 20 * 1000;
-
 test.describe('Map', () => {
-    test.skip('render stories <Default>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <Default>', async ({mount, expectScreenshot, page}) => {
         await mount(<Default />);
-        await delay(DEFAULT_MAP_DELAY);
-        await expectScreenshot({skipTheme: 'dark'});
+
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test.skip('render stories <Size>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <Size>', async ({mount, expectScreenshot, page}) => {
         await mount(<Size />);
-        await delay(DEFAULT_MAP_DELAY);
-        await expectScreenshot({skipTheme: 'dark'});
+
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test.skip('render stories <Direction>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <Direction>', async ({mount, expectScreenshot, page}) => {
         await mount(<Direction />);
-        await delay(DEFAULT_MAP_DELAY);
-        await expectScreenshot({skipTheme: 'dark'});
+
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test.skip('render stories <MapsTypes>', async ({mount, expectScreenshot, delay}) => {
+    test('render stories <MapsTypes>', async ({mount, expectScreenshot, page}) => {
         await mount(<MapsTypes />);
-        await delay(DEFAULT_MAP_DELAY);
-        await expectScreenshot({skipTheme: 'dark'});
+
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 });
