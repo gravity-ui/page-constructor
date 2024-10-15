79f29036f126d6c42b15bad2b83861f708dd08e7
daff <daff@yandex-team.ru>
2024-10-15 16:57:05 +0300

    feat: add zoom support for editor v2

--- src/editor-v2/context/iframeContext/store.ts
+++ src/editor-v2/context/iframeContext/store.ts
@@ -1,13 +1,19 @@
 import {ActionTypes, WithStoreReducer} from '../../../common/types';
+import {ZOOM_STEPS} from '../../constants';
 import {initializeStore} from '../../utils/store';
 
 export interface IframeState {
     url: string;
     height?: number;
+    // In percents
+    zoom: number;
 }
 
 export interface IframeMethods extends WithStoreReducer {
     setUrl: (url: string) => void;
+    setZoom: (zoom: number) => void;
+    increaseZoom: () => void;
+    decreaseZoom: () => void;
 }
 
 export type IframeStore = IframeState & IframeMethods;
@@ -16,8 +22,36 @@ export const createIframeStore = initializeStore<IframeState, IframeMethods>(
     {
         url: '',
         height: 400,
+        zoom: 100,
     },
-    (set) => ({
+    (set, get) => ({
+        setZoom: function (zoom) {
+            if (zoom > 0) {
+                set((state) => ({...state, zoom}));
+            }
+        },
+        increaseZoom: function () {
+            const currentZoom = get().zoom;
+
+            for (const step of ZOOM_STEPS) {
+                if (currentZoom < step) {
+                    get().setZoom(step);
+                    break;
+                }
+            }
+        },
+        decreaseZoom: function () {
+            const currentZoom = get().zoom;
+            const reverseSteps = ZOOM_STEPS.slice().reverse();
+
+            for (const step of reverseSteps) {
+                if (currentZoom > step) {
+                    get().setZoom(step);
+                    break;
+                }
+            }
+        },
+
         setUrl: (url) => set((state) => ({...state, url})),
         reducer: (action) => {
             switch (action.type) {
