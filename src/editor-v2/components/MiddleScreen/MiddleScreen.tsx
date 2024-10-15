5ea3e09c813a9fa3c141ad18317020363dc09869
daff <daff@yandex-team.ru>
2024-10-15 16:57:05 +0300

    feat: add zoom support for editor v2

--- src/editor-v2/components/MiddleScreen/MiddleScreen.tsx
+++ src/editor-v2/components/MiddleScreen/MiddleScreen.tsx
@@ -16,34 +16,48 @@ const b = block('middle-screen');
 interface MiddleScreenProps extends ClassNameProps {}
 
 const MiddleScreen: React.FC<MiddleScreenProps> = ({className}) => {
-    const {url, height} = useIframeStore();
+    const {url, height, zoom, decreaseZoom, increaseZoom, setZoom} = useIframeStore();
     const {initialized} = useEditorStore();
     const {setIframeElement} = useContext(IframeContext);
 
     return (
         <div className={b(null, className)}>
             <div className={b('topbar')}>
-                <TopBar />
-            </div>
-            <div className={b('canvas', {hidden: !initialized})}>
-                <iframe
-                    ref={(element) => {
-                        if (element) {
-                            setIframeElement(element);
-                        }
-                    }}
-                    className={b('iframe')}
-                    src={url}
-                    height={`${height}px`}
-                    width="100%"
-                    frameBorder="0"
+                <TopBar
+                    onZoomUpdate={setZoom}
+                    zoom={zoom}
+                    onDecreaseZoom={decreaseZoom}
+                    onIncreaseZoom={increaseZoom}
                 />
-                <Overlay className={b('overlay')} />
-                {!initialized && (
-                    <div className={b('loading')}>
-                        <Loader size={'l'} />
-                    </div>
-                )}
+            </div>
+            <div className={b('wrapper')}>
+                <div
+                    className={b('canvas', {hidden: !initialized})}
+                    style={{
+                        transform: `scale(${zoom}%)`,
+                        height: `${(100 / zoom) * 100}%`,
+                        width: `${(100 / zoom) * 100}%`,
+                    }}
+                >
+                    <iframe
+                        ref={(element) => {
+                            if (element) {
+                                setIframeElement(element);
+                            }
+                        }}
+                        className={b('iframe')}
+                        src={url}
+                        height={`${height}px`}
+                        width="100%"
+                        frameBorder="0"
+                    />
+                    <Overlay className={b('overlay')} />
+                    {!initialized && (
+                        <div className={b('loading')}>
+                            <Loader size={'l'} />
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
