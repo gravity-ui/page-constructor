a708243a3d8e58871e5f7cf88189041502a7f996
Sergey <148218517+qradle-yndx@users.noreply.github.com>
2024-11-27 12:58:04 +0300

    feat!: migrate to new slider, fix new slider styles (#978)
    * feat!: migrate to new slider
    
    ---------
    
    Co-authored-by: Nik Tverd <nn.tverd@gmail.com>
    Co-authored-by: gravity-ui-bot <111915794+gravity-ui-bot@users.noreply.github.com>
    Co-authored-by: Sergei Bubeev <qradle@qradle-dev.vla.yp-c.yandex.net>

--- src/blocks/Slider/utils.ts
+++ src/blocks/Slider/utils.ts
@@ -1,173 +1,73 @@
-import pickBy from 'lodash/pickBy';
 import * as React from 'react';
+
+import isEqual from 'lodash/isEqual';
+import pickBy from 'lodash/pickBy';
+import type {SwiperOptions} from 'swiper/types/swiper-options';
 
 import {BREAKPOINTS} from '../../constants';
 
 import {SliderBreakpointNames, SliderBreakpointParams, SlidesToShow} from './models';
 
 export const DEFAULT_SLIDE_BREAKPOINTS = {
-    [SliderBreakpointNames.Xl]: 3,
-    [SliderBreakpointNames.Lg]: 2,
+    [SliderBreakpointNames.Lg]: 3,
     [SliderBreakpointNames.Md]: 2,
-    [SliderBreakpointNames.Sm]: 1.15,
+    [SliderBreakpointNames.Sm]: 2,
+    [SliderBreakpointNames.Xs]: 1.15,
 };
-
-const BREAKPOINT_NAMES_BY_VALUES = Object.entries(BREAKPOINTS).reduce<
-    Record<number, SliderBreakpointNames>
->((acc, [key, value]) => ({...acc, [value]: key as SliderBreakpointNames}), {});
 
 export interface GetSlidesToShowParams {
     contentLength: number;
-    breakpoints?: SlidesToShow;
+    slidesToShow?: SlidesToShow;
     mobileFullscreen?: boolean;
 }
 
-export const isFocusable = (element: Element): boolean => {
-    if (!(element instanceof HTMLElement)) {
-        return false;
-    }
-    const tabIndexAttr = element.getAttribute('tabindex');
-    const hasTabIndex = tabIndexAttr !== null;
-    const tabIndex = Number(tabIndexAttr);
-    if (element.ariaHidden === 'true' || (hasTabIndex && tabIndex < 0)) {
-        return false;
-    }
-    if (hasTabIndex && tabIndex >= 0) {
-        return true;
-    }
-
-    // without this jest fails here for some reason
-    let htmlElement:
-        | HTMLAnchorElement
-        | HTMLInputElement
-        | HTMLSelectElement
-        | HTMLTextAreaElement
-        | HTMLButtonElement;
-    switch (true) {
-        case element instanceof HTMLAnchorElement:
-            htmlElement = element as HTMLAnchorElement;
-            return Boolean(htmlElement.href);
-        case element instanceof HTMLInputElement:
-            htmlElement = element as HTMLInputElement;
-            return htmlElement.type !== 'hidden' && !htmlElement.disabled;
-        case element instanceof HTMLSelectElement:
-        case element instanceof HTMLTextAreaElement:
-        case element instanceof HTMLButtonElement:
-            htmlElement = element as HTMLSelectElement | HTMLTextAreaElement | HTMLButtonElement;
-            return !htmlElement.disabled;
-        default:
-            return false;
-    }
-};
-
-export function getSlidesToShowWithDefaults({
+export function getSliderResponsiveParams({
     contentLength,
-    breakpoints,
+    slidesToShow,
     mobileFullscreen,
 }: GetSlidesToShowParams) {
     let result;
 
-    if (typeof breakpoints === 'number') {
+    if (typeof slidesToShow === 'number') {
         result = Object.keys(DEFAULT_SLIDE_BREAKPOINTS).reduce(
-            (acc, breakpointName) => ({...acc, [breakpointName]: breakpoints}),
+            (acc, breakpointName) => ({...acc, [breakpointName]: slidesToShow}),
             {} as SliderBreakpointParams,
         );
     } else {
-        result = breakpoints || DEFAULT_SLIDE_BREAKPOINTS;
+        result = slidesToShow || DEFAULT_SLIDE_BREAKPOINTS;
     }
 
-    return {
+    const showCount = {
         ...DEFAULT_SLIDE_BREAKPOINTS,
         ...pickBy(result, (value) => !isNaN(value)),
-        sm: !mobileFullscreen && contentLength > 1 ? DEFAULT_SLIDE_BREAKPOINTS.sm : 1,
+        xs: !mobileFullscreen && contentLength > 1 ? DEFAULT_SLIDE_BREAKPOINTS.xs : 1,
     };
+
+    return Object.entries(showCount).reduce(
+        (res, [breakpointName, value]) => {
+            // eslint-disable-next-line no-param-reassign
+            res[BREAKPOINTS[breakpointName as SliderBreakpointNames] + 1] = {
+                slidesPerView: value,
+            };
+            return res;
+        },
+        {} as Record<number, SwiperOptions>,
+    );
 }
 
-export function getSliderResponsiveParams(breakpoints: SliderBreakpointParams) {
-    return Object.entries(breakpoints).map(([breakpointName, slidesToShow]) => ({
-        breakpoint: BREAKPOINTS[breakpointName as SliderBreakpointNames],
-        settings: {slidesToShow},
-    }));
-}
-
-export function getSlidesCountByBreakpoint(
-    breakpoint: number,
-    breakpoints: SliderBreakpointParams,
-) {
-    const breakpointName = BREAKPOINT_NAMES_BY_VALUES[breakpoint];
-
-    return Math.floor(breakpoints[breakpointName]);
-}
-
-export function getSlidesToShowCount(breakpoints: SliderBreakpointParams) {
-    return Math.floor(Math.max(...Object.values(breakpoints)));
-}
-
-const getRovingListItemId = (uniqId: string, index: number) =>
-    `${uniqId}-roving-tabindex-item-${index}`;
-export function useRovingTabIndex(props: {
-    itemCount: number;
-    activeIndex: number;
-    firstIndex?: number;
-    uniqId: string;
-}) {
-    const {itemCount, activeIndex, firstIndex = 0, uniqId} = props;
-    const [currentIndex, setCurrentIndex] = React.useState(firstIndex);
-    const hasFocusRef = React.useRef(false);
-    const lastIndex = itemCount + firstIndex - 1;
-
-    const getRovingItemProps = (
-        index: number,
-    ): Pick<React.HTMLAttributes<HTMLElement>, 'id' | 'tabIndex' | 'onFocus'> => {
-        return {
-            id: getRovingListItemId(uniqId, index),
-            tabIndex: index === activeIndex ? 0 : -1,
-            onFocus: () => {
-                setCurrentIndex(index);
-                hasFocusRef.current = true;
-            },
-        };
-    };
+export const useMemoized = <T>(value: T): T => {
+    const [memoizedValue, setMemoizedValue] = React.useState(value);
 
     React.useEffect(() => {
-        if (!hasFocusRef.current) {
-            return;
-        }
-        document.getElementById(getRovingListItemId(uniqId, currentIndex))?.focus();
-    }, [activeIndex, currentIndex, uniqId]);
+        setMemoizedValue((memoized) =>
+            value && typeof value === 'object' && isEqual(memoized, value) ? memoized : value,
+        );
+    }, [value]);
 
-    const setNextIndex = () =>
-        setCurrentIndex((prev) => (prev >= lastIndex ? firstIndex : prev + 1));
-    const setPrevIndex = () =>
-        setCurrentIndex((prev) => (prev <= firstIndex ? lastIndex : prev - 1));
+    return memoizedValue;
+};
 
-    const onRovingListKeyDown: React.KeyboardEventHandler<HTMLElement> = (e) => {
-        const key = e.key.toLowerCase();
-
-        if (key !== 'tab' && key !== 'enter') {
-            e.preventDefault();
-        }
-
-        switch (key) {
-            case 'arrowleft':
-            case 'arrowup':
-                setPrevIndex();
-                return;
-            case 'arrowright':
-            case 'arrowdown':
-                setNextIndex();
-                return;
-        }
-    };
-
-    const onRovingListBlur: React.FocusEventHandler<HTMLElement> = () => {
-        hasFocusRef.current = false;
-    };
-
-    const rovingListProps: React.HTMLAttributes<HTMLElement> = {
-        onKeyDown: onRovingListKeyDown,
-        onBlur: onRovingListBlur,
-    };
-
-    return {getRovingItemProps, rovingListProps};
-}
+export const setElementAtrributes = (element: Element, attributes: Record<string, unknown>) =>
+    Object.entries(attributes).forEach(([attribute, value]) =>
+        element.setAttribute(attribute, String(value)),
+    );
---
+++ src/blocks/SliderOld/__tests__/Slider.test.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import {queryHelpers, render} from '@testing-library/react';
+
+import {BasicCard} from '../../../sub-blocks';
+import Slider from '../SliderOld';
+
+const EXAMPLE_URL = 'https://example.com';
+const SLIDER_TITLE = 'Slider title';
+const CARD_TITLE = 'Card title';
+const CARD_TEXT = 'Lorem ipsum';
+const CARDS_COUNT = 10;
+
+const slidesToShowValues = [3, 2, 1];
+
+describe('Slider', () => {
+    test.each(slidesToShowValues)('Has correct slider labels', async (slidesToShow) => {
+        const {container} = render(
+            <Slider title={{text: SLIDER_TITLE, url: EXAMPLE_URL}} slidesToShow={slidesToShow}>
+                {Array(CARDS_COUNT)
+                    .fill(null)
+                    .map((_, index) => (
+                        <BasicCard
+                            url={EXAMPLE_URL}
+                            title={CARD_TITLE}
+                            text={CARD_TEXT}
+                            key={index}
+                        />
+                    ))}
+            </Slider>,
+        );
+
+        // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+        const barElement = container.querySelector('.pc-SliderOldBlock__bar');
+        if (slidesToShow > 1) {
+            expect(barElement).toBeTruthy();
+        } else {
+            expect(barElement).toBeFalsy();
+        }
+
+        const barDotsCount = CARDS_COUNT - slidesToShow + 1;
+
+        // Checking labels for the first slide
+        // There we have a bar covering `slidesToShow` dots
+        // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+        const accessibleBarElement = container.querySelector('.pc-SliderOldBlock__accessible-bar');
+        expect(accessibleBarElement?.getAttribute('aria-label')).toBe(`Page 1 of ${barDotsCount}`);
+        expect(
+            queryHelpers.queryAllByAttribute('aria-label', container, `Page 1 of ${barDotsCount}`),
+        ).toHaveLength(slidesToShow + 1);
+
+        // Checking labels for the slides starting from 2
+        Array(barDotsCount - 1)
+            .fill(null)
+            .forEach((_, index) => {
+                expect(
+                    queryHelpers.queryAllByAttribute(
+                        'aria-label',
+                        container,
+                        `Page ${index + 2} of ${barDotsCount}`,
+                    ),
+                ).toHaveLength(1);
+            });
+    });
+});
