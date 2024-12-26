72bb781a67799160c04a4fb004fc2bb4a719eef7
qradle <qradle@yandex-team.ru>
2024-12-26 17:18:27 +0300

    fix: add delay to Map visual tests

--- src/blocks/Map/__tests__/Map.visual.test.tsx
+++ src/blocks/Map/__tests__/Map.visual.test.tsx
@@ -4,28 +4,30 @@ import {test} from '../../../../playwright/core/index';
 
 import {Default, Direction, MapsTypes, Size} from './helpers';
 
+const DEFAULT_MAP_DELAY = 10 * 1000;
+
 test.describe('Map', () => {
-    test('render stories <Default>', async ({mount, expectScreenshot, page}) => {
+    test('render stories <Default>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<Default />);
-
+        await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test('render stories <Size>', async ({mount, expectScreenshot, page}) => {
+    test('render stories <Size>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<Size />);
-
+        await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test('render stories <Direction>', async ({mount, expectScreenshot, page}) => {
+    test('render stories <Direction>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<Direction />);
-
+        await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 
-    test('render stories <MapsTypes>', async ({mount, expectScreenshot, page}) => {
+    test('render stories <MapsTypes>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<MapsTypes />);
-
+        await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
 });
