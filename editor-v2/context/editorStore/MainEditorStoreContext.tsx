c45c5dc8b6b6ebf9193a5ef44498fd68b548dd2d
qradle <qradle@yandex-team.ru>
2025-03-04 18:23:56 +0300

    refactor: distinct editor v2, add components config

---
+++ editor-v2/context/editorStore/MainEditorStoreContext.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StoreApi} from 'zustand';
+
+import {EditorStore, createEditorStore} from '../../store';
+
+export interface MainEditorStoreContextProps {
+    state: StoreApi<EditorStore>;
+}
+
+export const MainEditorStoreContext = React.createContext<MainEditorStoreContextProps>({
+    state: createEditorStore(),
+});
--- playground/src/app/pc-2/page.tsx
+++ playground/src/app/pc-2/page.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 import {PageConstructor, PageConstructorProvider} from '../../../../src';
 
 import content from './content.json';
+import navigation from './navigation.json';
 import './styles.scss';
 
 export default function Home() {
     return (
         <PageConstructorProvider projectSettings={{disableCompress: true}}>
-            <PageConstructor content={content} />
+            <PageConstructor content={content} navigation={navigation} />
         </PageConstructorProvider>
     );
 }
