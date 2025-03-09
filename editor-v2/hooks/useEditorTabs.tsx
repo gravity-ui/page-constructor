d177d3ba4dca9914f23e9d6554cbf7f8d82736b7
qradle <qradle@yandex-team.ru>
2025-03-10 00:16:29 +0300

    feat: tree selected block style

--- editor-v2/hooks/useEditorTabs.tsx
+++ editor-v2/hooks/useEditorTabs.tsx
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 import Tabs, {TabsItemProps} from '../components/Tabs/Tabs';
 import BlockConfig from '../containers/BlockConfig/BlockConfig';
 import BlocksList from '../containers/BlocksList/BlocksList';
@@ -12,63 +14,68 @@ export const useEditorTabs = ({
     leftTabs?: TabsItemProps[];
     rightTabs?: TabsItemProps[];
 }) => {
-    return {
-        left: [
-            {
-                id: 'page',
-                title: 'Page',
-                component: () => (
-                    <Tabs
-                        items={[
-                            {
-                                id: 'blocks-list',
-                                title: 'Blocks',
-                                component: BlocksList,
-                            },
-                            {
-                                id: 'tree',
-                                title: 'Tree',
-                                component: Tree,
-                            },
-                            {
-                                id: 'source-code-yaml',
-                                title: 'YAML',
-                                component: () => <SourceCode format="yaml" />,
-                            },
-                            {
-                                id: 'source-code-json',
-                                title: 'JSON',
-                                component: () => <SourceCode format="json" />,
-                            },
-                        ]}
-                    />
-                ),
-            },
-            {
-                id: 'global-config',
-                title: 'GlobalConfig',
-                component: GlobalConfig,
-            },
-            ...leftTabs,
-        ],
-        right: [
-            {
-                id: 'block-config',
-                title: 'BlockConfig',
-                component: BlockConfig,
-                position: 'right',
-            },
-            {
-                id: 'source-code-yaml',
-                title: 'YAML',
-                component: () => <SourceCode format="yaml" />,
-            },
-            {
-                id: 'source-code-json',
-                title: 'JSON',
-                component: () => <SourceCode format="json" />,
-            },
-            ...rightTabs,
-        ],
-    };
+    const tabs = useMemo(
+        () => ({
+            left: [
+                {
+                    id: 'page',
+                    title: 'Page',
+                    component: () => (
+                        <Tabs
+                            items={[
+                                {
+                                    id: 'blocks-list',
+                                    title: 'Blocks',
+                                    component: BlocksList,
+                                },
+                                {
+                                    id: 'tree',
+                                    title: 'Tree',
+                                    component: Tree,
+                                },
+                                {
+                                    id: 'source-code-yaml',
+                                    title: 'YAML',
+                                    component: () => <SourceCode format="yaml" />,
+                                },
+                                {
+                                    id: 'source-code-json',
+                                    title: 'JSON',
+                                    component: () => <SourceCode format="json" />,
+                                },
+                            ]}
+                        />
+                    ),
+                },
+                {
+                    id: 'global-config',
+                    title: 'GlobalConfig',
+                    component: GlobalConfig,
+                },
+                ...leftTabs,
+            ],
+            right: [
+                {
+                    id: 'block-config',
+                    title: 'BlockConfig',
+                    component: BlockConfig,
+                    position: 'right',
+                },
+                {
+                    id: 'source-code-yaml',
+                    title: 'YAML',
+                    component: () => <SourceCode format="yaml" />,
+                },
+                {
+                    id: 'source-code-json',
+                    title: 'JSON',
+                    component: () => <SourceCode format="json" />,
+                },
+                ...rightTabs,
+            ],
+        }),
+        [],
+    );
+
+    return tabs;
 };
