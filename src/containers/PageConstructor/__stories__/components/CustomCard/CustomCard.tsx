ea1d5f8a0cc7334f1eec674fbd979bca1a3cddcd
PahaN47 <pahan47@yandex-team.ru>
2025-03-21 11:13:23 +0300

    fix: custom card bug

--- src/containers/PageConstructor/__stories__/components/CustomCard/CustomCard.tsx
+++ src/containers/PageConstructor/__stories__/components/CustomCard/CustomCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import * as React from 'react';
 import {ArrowUturnCcwLeft} from '@gravity-ui/icons';
 import {CardBase, YFMWrapper} from '../../../../../components';
@@ -45,52 +47,55 @@ export const CustomCard = ({
     const flip = () => setIsFlipped((prev) => !prev);
 
     return (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-        <div className={b({flipped: isFlipped})} onClick={flip}>
-            <CardBase className={b('card', {bottom: true})} bodyClassName={b('card-body')}>
-                <CardBase.Header className={b('card-header')}>
-                    <div className={b('card-header-content')}>
-                        <h2 className={b('card-header-content-title')}>{title}</h2>
-                        <ArrowUturnCcwLeft
-                            className={b('card-header-content-flip-icon')}
-                            width={20}
-                            height={20}
-                        />
-                    </div>
-                </CardBase.Header>
-                <CardBase.Content>
-                    <Link
-                        className={b('card-body-link')}
-                        href={url ?? ''}
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        <YFMWrapper
-                            className={b('card-body-link-code')}
-                            content={`<code>${content}</code>`}
-                        />
-                    </Link>
-                </CardBase.Content>
-            </CardBase>
-            <CardBase className={b('card')} bodyClassName={b('card-body')}>
-                <CardBase.Header className={b('card-header')}>
-                    <div className={b('card-header-content')}>
-                        <h2 className={b('card-header-content-title')}>{title}</h2>
-                        <ArrowUturnCcwLeft
-                            className={b('card-header-content-flip-icon')}
-                            width={20}
-                            height={20}
-                        />
-                    </div>
-                </CardBase.Header>
-                <CardBase.Content>
-                    {description && (
-                        <YFMWrapper
-                            className={b('card-body-description')}
-                            content={yfmTransform(description)}
-                        />
-                    )}
-                </CardBase.Content>
-            </CardBase>
+        <div className={b({flipped: isFlipped})}>
+            <div className={b('card-wrap', {bottom: true})} onClick={flip}>
+                <CardBase className={b('card')} bodyClassName={b('card-body')}>
+                    <CardBase.Header className={b('card-header')}>
+                        <div className={b('card-header-content')}>
+                            <h2 className={b('card-header-content-title')}>{title}</h2>
+                            <ArrowUturnCcwLeft
+                                className={b('card-header-content-flip-icon')}
+                                width={20}
+                                height={20}
+                            />
+                        </div>
+                    </CardBase.Header>
+                    <CardBase.Content>
+                        <Link
+                            className={b('card-body-link')}
+                            href={url ?? ''}
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <YFMWrapper
+                                className={b('card-body-link-code')}
+                                content={`<code>${content}</code>`}
+                            />
+                        </Link>
+                    </CardBase.Content>
+                </CardBase>
+            </div>
+            <div className={b('card-wrap')} onClick={flip}>
+                <CardBase className={b('card')} bodyClassName={b('card-body')}>
+                    <CardBase.Header className={b('card-header')}>
+                        <div className={b('card-header-content')}>
+                            <h2 className={b('card-header-content-title')}>{title}</h2>
+                            <ArrowUturnCcwLeft
+                                className={b('card-header-content-flip-icon')}
+                                width={20}
+                                height={20}
+                            />
+                        </div>
+                    </CardBase.Header>
+                    <CardBase.Content>
+                        {description && (
+                            <YFMWrapper
+                                className={b('card-body-description')}
+                                content={yfmTransform(description)}
+                            />
+                        )}
+                    </CardBase.Content>
+                </CardBase>
+            </div>
         </div>
     );
 };
