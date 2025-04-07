1f04272ce53395521ebfb0e9bb8fb8c7e271109c
qradle <qradle@yandex-team.ru>
2025-04-07 20:24:49 +0300

    fix: update editor-v2 exports

---
+++ src/editor-v2/containers/index.ts
@@ -0,0 +1,7 @@
+export {Editor} from './Editor/Editor';
+export {default as BlockConfigForm} from './BlockConfigForm/BlockConfigForm';
+export {default as BlocksList} from './BlocksList/BlocksList';
+export {default as Source} from './Source/Source';
+export {default as SourceCode} from './SourceCode/SourceCode';
+export {default as Tree} from './Tree/Tree';
+export {default as ViewSwitches} from './ViewSwitches/ViewSwitches';
