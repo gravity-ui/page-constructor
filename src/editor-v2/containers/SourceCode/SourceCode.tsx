558bbf84b9c8cc705a34a1c2404077a2b017209e
qradle <qradle@yandex-team.ru>
2025-04-28 14:10:25 +0300

    fix: json/yaml tab for selected block

--- src/editor-v2/containers/SourceCode/SourceCode.tsx
+++ src/editor-v2/containers/SourceCode/SourceCode.tsx
@@ -1,10 +1,12 @@
 import {ArrowDownToSquare} from '@gravity-ui/icons';
 import {Button, ClipboardButton, Icon} from '@gravity-ui/uikit';
 import yaml from 'js-yaml';
+import _ from 'lodash';
 import * as React from 'react';
 
 import {PageContentWithNavigation} from '../../../models';
 import {useMainEditorStore} from '../../hooks/useMainEditorStore';
+import {generateChildrenPathFromArray} from '../../utils';
 import {editorCn} from '../../utils/cn';
 
 import './SourceCode.scss';
@@ -15,36 +17,58 @@ const b = editorCn('source-code');
 interface SourceCodeProps {
     className?: string;
     format: 'yaml' | 'json';
+    showSelectedBlockOnly?: boolean;
 }
 
-const SourceCode = ({className, format}: SourceCodeProps) => {
-    const {content, setContent} = useMainEditorStore();
+const SourceCode = ({className, format, showSelectedBlockOnly = false}: SourceCodeProps) => {
+    const {content, setContent, selectedBlock} = useMainEditorStore();
     const [isOpen, setIsOpen] = React.useState(false);
 
     const handleUpdate = (tempConfig: string) => {
-        let object: PageContentWithNavigation | undefined;
+        let object: any;
 
         try {
             if (tempConfig.trim().startsWith('{') && tempConfig.trim().endsWith('}')) {
                 object = JSON.parse(tempConfig);
             } else {
-                object = yaml.load(tempConfig) as PageContentWithNavigation;
+                object = yaml.load(tempConfig);
             }
         } catch {
             // eslint-disable-next-line no-console
             console.error('JSON.parse failed');
+            return;
         }
 
-        if (object) {
-            setContent(object);
+        if (showSelectedBlockOnly && selectedBlock) {
+            // Update only the selected block
+            const currentBlockPath = generateChildrenPathFromArray(selectedBlock);
+            const newContent = _.cloneDeep(content);
+            _.set(newContent.blocks, currentBlockPath, object);
+            setContent(newContent);
+        } else if (object) {
+            // Update the entire content
+            setContent(object as PageContentWithNavigation);
         }
 
         setIsOpen(false);
     };
 
     const textContent = React.useMemo(() => {
+        if (showSelectedBlockOnly && selectedBlock) {
+            const currentBlockPath = generateChildrenPathFromArray(selectedBlock);
+            const currentConfig = _.get(content.blocks, currentBlockPath || '');
+
+            if (currentConfig) {
+                return format === 'yaml'
+                    ? yaml.dump(currentConfig)
+                    : JSON.stringify(currentConfig, null, 2);
+            }
+
+            return 'No block selected';
+        }
+
         return format === 'yaml' ? yaml.dump(content) : JSON.stringify(content, null, 2);
-    }, [format, content]);
+    }, [format, content, showSelectedBlockOnly, selectedBlock]);
 
     return (
         <div className={b(null, className)}>
