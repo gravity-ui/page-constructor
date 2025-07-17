a11eff1eb95515728e44638b2253a85ec5cd2581
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2025-07-17 18:00:25 +0300

    fix: add all text sizes to validator (#1239)

--- src/schema/validators/common.ts
+++ src/schema/validators/common.ts
@@ -10,7 +10,7 @@ import {
 import {AnalyticsEventSchema} from './event';
 
 export const mediaDirection = ['media-content', 'content-media'];
-export const textSize = ['s', 'm', 'l'];
+export const textSize = ['xs', 's', 'sm', 'm', 'l'];
 export const containerSizesArray = ['sm', 'md', 'lg', 'xl', 'all'];
 export const sliderSizesArray = ['sm', 'md', 'lg', 'xl'];
 export const contentSizes = ['s', 'm', 'l'];
