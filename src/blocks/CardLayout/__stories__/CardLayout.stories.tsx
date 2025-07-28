986df7249c245d7f8bfdab9b24954242545f0a93
qradle <qradle@yandex-team.ru>
2025-07-29 05:06:35 +0800

    chore(CardLayout): update storybook, screenshots

--- src/blocks/CardLayout/__stories__/CardLayout.stories.tsx
+++ src/blocks/CardLayout/__stories__/CardLayout.stories.tsx
@@ -1,21 +1,43 @@
+import * as React from 'react';
 import {Meta, StoryFn} from '@storybook/react';
 
-import {yfmTransformInline} from '../../../../.storybook/utils';
-import {PageConstructor} from '../../../containers/PageConstructor';
-import {CardLayoutBlockModel, SubBlockModels} from '../../../models';
-import CardLayout from '../CardLayout';
+import {blockTransform} from '../../../../.storybook/utils';
+import {
+    BackgroundCardProps,
+    BasicCardProps,
+    CardLayoutBlockModel,
+    ImageCardProps,
+    LayoutItemProps,
+    PriceCardProps,
+    SubBlockModels,
+} from '../../../models';
+import {BackgroundCard, BasicCard, ImageCard, LayoutItem, PriceCard} from '../../../sub-blocks';
+import CardLayout, {CardLayoutBlockProps} from '../CardLayout';
 
 import data from './data.json';
 
 export default {
     title: 'Blocks/CardLayout',
     component: CardLayout,
-    parameters: {
-        controls: {
-            include: ['blocks'],
-        },
-    },
 } as Meta;
+
+const renderChild = (childArgs: SubBlockModels, index?: number) => {
+    const childProps = blockTransform(childArgs);
+    switch (childArgs.type) {
+        case 'basic-card':
+            return <BasicCard key={index} {...(childProps as BasicCardProps)} />;
+        case 'layout-item':
+            return <LayoutItem key={index} {...(childProps as LayoutItemProps)} />;
+        case 'background-card':
+            return <BackgroundCard key={index} {...(childProps as BackgroundCardProps)} />;
+        case 'price-card':
+            return <PriceCard key={index} {...(childProps as PriceCardProps)} />;
+        case 'image-card':
+            return <ImageCard key={index} {...(childProps as ImageCardProps)} />;
+        default:
+            return null;
+    }
+};
 
 const createCardArray: (
     count: number,
@@ -23,15 +45,43 @@ const createCardArray: (
 ) => SubBlockModels[] = (count, shared) =>
     Array.from({length: count}, () => ({...shared}) as SubBlockModels);
 
-const DefaultTemplate: StoryFn<{blocks: CardLayoutBlockModel[]}> = ({blocks}) => {
-    return <PageConstructor content={{blocks}} />;
+const DefaultTemplate: StoryFn<CardLayoutBlockModel> = ({children = [], ...args}) => {
+    return (
+        <div style={{padding: '48px'}}>
+            <CardLayout {...(blockTransform(args) as CardLayoutBlockProps)} animated={false}>
+                {children.map(renderChild)}
+            </CardLayout>
+        </div>
+    );
 };
 
+const MultipleTemplate: StoryFn<{blocks: CardLayoutBlockModel[]}> = ({blocks}) => (
+    <React.Fragment>
+        {blocks.map(({children = [], ...itemArgs}, index) => (
+            <div key={index} style={{padding: '48px'}}>
+                <CardLayout
+                    {...(itemArgs as CardLayoutBlockProps)}
+                    animated={false}
+                    className="card-layout-stories"
+                >
+                    {children.map(renderChild)}
+                </CardLayout>
+            </div>
+        ))}
+    </React.Fragment>
+);
+
 export const Default = DefaultTemplate.bind({});
-export const ColSize = DefaultTemplate.bind({});
-export const WithBackground = DefaultTemplate.bind({});
+export const ChildrenCardOptions = MultipleTemplate.bind({});
+export const ColSize = MultipleTemplate.bind({});
+export const WithBackground = MultipleTemplate.bind({});
 
 Default.args = {
+    ...data.default.content,
+    children: createCardArray(6, data.cards.basicCard),
+} as CardLayoutBlockModel;
+
+ChildrenCardOptions.args = {
     blocks: [
         {
             ...data.default.content,
@@ -53,17 +103,14 @@ Default.args = {
             children: [
                 {
                     ...data.cards.priceCard,
-                    title: yfmTransformInline(data.cards.priceCard.title),
                     buttons: [data.buttons.outlined],
                 },
                 {
                     ...data.cards.priceCard,
-                    title: yfmTransformInline(data.cards.priceCard.title),
                     buttons: [data.buttons.action],
                 },
                 {
                     ...data.cards.priceCard,
-                    title: yfmTransformInline(data.cards.priceCard.title),
                     buttons: [data.buttons.monochrome],
                 },
             ],
@@ -73,7 +120,12 @@ Default.args = {
             title: 'Card layout with image cards',
             children: createCardArray(3, data.cards.imageCard),
         },
-    ] as CardLayoutBlockModel[],
+    ],
+} as {blocks: CardLayoutBlockModel[]};
+ChildrenCardOptions.parameters = {
+    controls: {
+        include: ['blocks'],
+    },
 };
 
 ColSize.args = {
@@ -91,6 +143,11 @@ ColSize.args = {
             children: createCardArray(8, data.colSizes.two.card),
         },
     ] as CardLayoutBlockModel[],
+};
+ColSize.parameters = {
+    controls: {
+        include: ['blocks'],
+    },
 };
 
 WithBackground.args = {
@@ -120,4 +177,9 @@ WithBackground.args = {
             children: createCardArray(4, data.cards.priceCard),
         },
     ] as CardLayoutBlockModel[],
+};
+WithBackground.parameters = {
+    controls: {
+        include: ['blocks'],
+    },
 };
