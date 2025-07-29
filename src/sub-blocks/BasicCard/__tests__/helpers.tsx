15abeed2487991a207736eaf91c786be47fc5468
aeksandla <aeksandla@yandex-team.ru>
2025-07-29 10:48:31 +0300

    feat(ContentList)(BasicCard): add screenshots

--- src/sub-blocks/BasicCard/__tests__/helpers.tsx
+++ src/sub-blocks/BasicCard/__tests__/helpers.tsx
@@ -2,5 +2,13 @@ import {composeStories} from '@storybook/react';
 
 import * as BasicCardStories from '../__stories__/BasicCard.stories';
 
-export const {Default, WithIcon, WithBorder, WithUrl, WithContentList, ControlPosition, Sizes} =
-    composeStories(BasicCardStories);
+export const {
+    Default,
+    WithIcon,
+    WithBorder,
+    WithUrl,
+    WithContentList,
+    ControlPosition,
+    Sizes,
+    GravityIcons,
+} = composeStories(BasicCardStories);
