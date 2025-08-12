f8348c5fc43faa65f1076c39cb5fb1e59c62aa76
DaffPunks <sawavas2@gmail.com>
2025-08-12 16:48:42 +0300

    fix: use @bem-react/classname in PC Form Generator

--- src/form-generator/components/SectionHeader/SectionHeader.tsx
+++ src/form-generator/components/SectionHeader/SectionHeader.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import {Text} from '@gravity-ui/uikit';
-import block from 'bem-cn-lite';
+import {formGeneratorCn} from '../../utils/cn';
 
 import './SectionHeader.scss';
 
-const b = block('section-header');
+const b = formGeneratorCn('section-header');
 
 interface SectionHeaderProps {
     title: string;
