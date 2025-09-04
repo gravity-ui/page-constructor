1894db737297cc6a7cba6706e7d95ee962355888
aeksandla <aeksandla@yandex-team.ru>
2025-09-04 16:19:43 +0300

    chore: update buttons story

--- src/components/Button/__stories__/Button.stories.tsx
+++ src/components/Button/__stories__/Button.stories.tsx
@@ -1,7 +1,8 @@
-import {IconData} from '@gravity-ui/uikit';
+import * as React from 'react';
+
 import {Meta, StoryFn} from '@storybook/react';
 
-import {Col, Grid, Row} from '../../../grid';
+import {Col, Grid, GridAlignItems, Row} from '../../../grid';
 import Button, {ButtonProps} from '../Button';
 
 import {CONTRAST_THEMES, SIZES, THEMES} from './constants';
@@ -13,177 +14,134 @@ import './Button.stories.scss';
 export default {
     component: Button,
     title: 'Components/Links and buttons/Button',
-    args: {
-        text: 'Button',
-        size: 'l',
-    },
-    argTypes: {
-        size: {
-            control: {
-                type: 'select',
-            },
-            options: SIZES,
-        },
-    },
 } as Meta;
 
-const createIconConfig = (
-    iconData: IconData,
-    position?: 'left' | 'right',
-    iconSize?: number,
-    className?: string,
-) => ({iconData, position, iconSize, className});
-
-const createButtonRow = (label: string, buttons: React.ReactNode[]) => (
-    <Row>
-        <Col style={{margin: '20px 0'}}>{label}</Col>
-        {buttons.map((button, index) => (
-            <Col key={index}>{button}</Col>
-        ))}
-    </Row>
+const THEMES_SIZES = [...THEMES, ...CONTRAST_THEMES].map((theme) =>
+    SIZES.map((size) => ({
+        size,
+        theme,
+        ...data.themesSizes,
+    })),
 );
+
+const ICON_VARIANTS = [
+    {iconData: 'Plus'},
+    {iconData: data.iconExamples.signatureSvg},
+    {url: data.iconExamples.customUrl},
+].map((source) =>
+    [{position: 'left'}, {position: 'right'}, {iconSize: 14}, {iconSize: 20}].map((props) => ({
+        img: {
+            ...source,
+            ...props,
+        },
+        ...data.withIcon,
+    })),
+);
+const ICON_SOURCES = ['Gravity UI', 'Custom SVG', 'Img URL'];
 
 const DefaultTemplate: StoryFn<ButtonProps> = (args) => <Button {...args} />;
 
-const SizesTemplate: StoryFn<ButtonProps> = (args) => (
-    <Row>
-        <Col style={{margin: '20px 0'}}>{args.theme}</Col>
-        {SIZES.map((size) => (
-            <Col key={size}>
-                <Button {...args} size={size} />
+const SizesTemplate: StoryFn<Record<number, ButtonProps>> = (args) => (
+    <Row
+        alignItems={GridAlignItems.End}
+        style={{
+            background: CONTRAST_THEMES.includes(args[0].theme as (typeof CONTRAST_THEMES)[number])
+                ? 'rgb(58, 116, 255)'
+                : 'transparent',
+            padding: '10px 0',
+        }}
+    >
+        <Col>{args[0].theme}</Col>
+        {Object.values(args).map((itemArgs, index) => (
+            <Col key={index}>
+                <Button {...itemArgs} />
             </Col>
         ))}
     </Row>
 );
 
-const ThemesSizesTemplate: StoryFn<ButtonProps> = (args) => (
-    <Grid>
-        <Row>
-            <Col />
-            {SIZES.map((size) => (
-                <Col key={size}>{size}</Col>
+const ThemesSizesTemplate: StoryFn<Record<number, Record<number, ButtonProps>>> = (args) => (
+    <React.Fragment>
+        <Grid>
+            <Row>
+                <Col />
+                {Object.values(args[0]).map((arg, index) => (
+                    <Col key={index}>{arg.size}</Col>
+                ))}
+            </Row>
+            {Object.values(args).map((arg, index) => (
+                <SizesTemplate key={index} {...arg} />
             ))}
-        </Row>
-        {THEMES.map((theme) => (
-            <SizesTemplate key={theme} {...args} theme={theme} />
-        ))}
-        <section style={{color: '#fff', background: '#3a74ff'}}>
-            {CONTRAST_THEMES.map((theme) => (
-                <SizesTemplate key={theme} {...args} theme={theme} />
-            ))}
-        </section>
-    </Grid>
+        </Grid>
+    </React.Fragment>
 );
 
-const WidthTemplate: StoryFn<ButtonProps> = (args) => (
+const VariantsTemplate: StoryFn<Record<number, ButtonProps>> = (args) => (
     <Row>
-        <Col>
-            <Button {...args} width="auto" />
-        </Col>
-        <Col>
-            <Button {...args} width="max" />
-        </Col>
+        {Object.values(args).map((arg, index) => (
+            <Col key={index}>
+                <Button {...arg} />
+            </Col>
+        ))}
     </Row>
 );
 
-const IconTemplate: StoryFn<ButtonProps> = (args) => {
-    const {iconExamples} = data;
-
-    return (
+const IconTemplate: StoryFn<Record<number, Record<number, ButtonProps>>> = (args) => (
+    <React.Fragment>
         <Grid>
             <Row>
-                <Col>Source</Col>
+                <Col />
                 <Col>Left</Col>
                 <Col>Right</Col>
                 <Col>Small</Col>
                 <Col>Large</Col>
             </Row>
-
-            {createButtonRow('Gravity UI', [
-                <Button key="plus-left" {...args} img={createIconConfig('Plus')} text="Button" />,
-                <Button
-                    key="plus-right"
-                    {...args}
-                    img={createIconConfig('Plus', 'right')}
-                    text="Button"
-                />,
-                <Button
-                    key="plus-small"
-                    {...args}
-                    img={createIconConfig('Plus', 'left', 14)}
-                    text="Button"
-                />,
-                <Button
-                    key="plus-large"
-                    {...args}
-                    img={createIconConfig('Plus', 'left', 20)}
-                    text="Button"
-                />,
-            ])}
-
-            {createButtonRow('Custom SVG', [
-                <Button
-                    key="signature-left"
-                    {...args}
-                    img={createIconConfig(iconExamples.signatureSvg)}
-                    text="Button"
-                />,
-                <Button
-                    key="signature-right"
-                    {...args}
-                    img={createIconConfig(iconExamples.signatureSvg, 'right')}
-                    text="Button"
-                />,
-                <Button
-                    key="signature-small"
-                    {...args}
-                    img={createIconConfig(iconExamples.signatureSvg, 'left', 14)}
-                    text="Button"
-                />,
-                <Button
-                    key="signature-large"
-                    {...args}
-                    img={createIconConfig(iconExamples.signatureSvg, 'left', 20)}
-                    text="Button"
-                />,
-            ])}
-
-            {createButtonRow('Img URL', [
-                <Button
-                    key="gravity-left"
-                    {...args}
-                    img={{url: iconExamples.customUrl, position: 'left'}}
-                    text="Button"
-                />,
-                <Button
-                    key="gravity-right"
-                    {...args}
-                    img={{url: iconExamples.customUrl, position: 'right'}}
-                    text="Button"
-                />,
-                <Button
-                    key="gravity-small"
-                    {...args}
-                    img={{url: iconExamples.customUrl, position: 'left', iconSize: 14}}
-                    text="Button"
-                />,
-                <Button
-                    key="gravity-large"
-                    {...args}
-                    img={{url: iconExamples.customUrl, position: 'left', iconSize: 20}}
-                    text="Button"
-                />,
-            ])}
+            {Object.values(args).map((arg, index) => (
+                <Row
+                    key={index}
+                    alignItems={GridAlignItems.End}
+                    style={{
+                        background: CONTRAST_THEMES.includes(
+                            arg[0].theme as (typeof CONTRAST_THEMES)[number],
+                        )
+                            ? 'rgb(58, 116, 255)'
+                            : 'transparent',
+                        padding: '10px 0',
+                    }}
+                >
+                    <Col>{ICON_SOURCES[index]}</Col>
+                    {Object.values(arg).map((itemArgs, indexInner) => (
+                        <Col key={indexInner}>
+                            <Button {...itemArgs} />
+                        </Col>
+                    ))}
+                </Row>
+            ))}
         </Grid>
-    );
-};
+    </React.Fragment>
+);
 
 export const Default = DefaultTemplate.bind({});
-export const ThemesSizes = ThemesSizesTemplate.bind({});
-export const Width = WidthTemplate.bind({});
-export const Icons = IconTemplate.bind({});
+export const ThemesSizes = ThemesSizesTemplate.bind([[]]);
+ThemesSizes.parameters = {
+    controls: {
+        include: Object.keys(THEMES_SIZES),
+    },
+};
+export const Width = VariantsTemplate.bind([]);
+Width.parameters = {
+    controls: {
+        include: Object.keys(data.width),
+    },
+};
+export const Icons = IconTemplate.bind([[]]);
+Icons.parameters = {
+    controls: {
+        include: Object.keys(ICON_VARIANTS),
+    },
+};
 
-Default.args = data.default.content as ButtonProps;
-ThemesSizes.args = data.themesSizes.content as ButtonProps;
-Width.args = data.themesSizes.content as ButtonProps;
-Icons.args = data.withIcon.content as ButtonProps;
+Default.args = data.default as ButtonProps;
+ThemesSizes.args = THEMES_SIZES as ButtonProps[][];
+Width.args = data.width as ButtonProps[];
+Icons.args = ICON_VARIANTS as ButtonProps[][];
