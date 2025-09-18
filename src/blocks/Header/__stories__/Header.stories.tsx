71ac3d24ed78f5ca05de6b3a7b431061ffaceeba
Aleksandra Pleshcheva <46083995+aeksandla@users.noreply.github.com>
2025-09-18 13:40:25 +0300

    chore(Header): update story (#1290)

--- src/blocks/Header/__stories__/Header.stories.tsx
+++ src/blocks/Header/__stories__/Header.stories.tsx
@@ -2,267 +2,154 @@ import * as React from 'react';
 
 import {Meta, StoryFn} from '@storybook/react';
 
-import {yfmTransform, yfmTransformInline} from '../../../../.storybook/utils';
-import {PageConstructor} from '../../../containers/PageConstructor';
-import {ButtonProps, HeaderBlockModel, HeaderBlockProps} from '../../../models';
+import {blockTransform} from '../../../../.storybook/utils';
+import {HeaderBlockModel, HeaderBlockProps} from '../../../models';
 import Header from '../Header';
 
 import data from './data.json';
 
-type HeaderBlockPropsNoTitle = Omit<HeaderBlockProps, 'title'>;
-
-const getSizeTitle = (size: string) =>
-    yfmTransformInline(data.size.title.replace('{{size}}', size));
-const getImageTitle = (text: string) =>
-    yfmTransformInline(data.image.title.replace('{{text}}', text));
-const getVerticalOffsetTitle = (offset: string) =>
-    yfmTransformInline(data.verticalOffset.title.replace('{{offset}}', offset));
-const getBreadcrumbsTitle = (theme: string) =>
-    yfmTransformInline(data.breadcrumbs.title.replace('{{theme}}', theme));
+const SIZES = ['l', 'm', 's'].map((width) => ({
+    width,
+    ...data.default,
+    title: data.size.title.replace('{{size}}', width),
+}));
 
 export default {
     title: 'Blocks/Header',
     component: Header,
-    args: {
-        image: undefined,
-        video: undefined,
-        background: undefined,
-        breadcrumbs: undefined,
-        offset: 'default',
-        theme: 'light',
-        verticalOffset: 'm',
-        centered: undefined,
-    },
-    argTypes: {
-        overtitle: {control: 'text'},
-    },
 } as Meta;
 
-const DefaultArgs = {
-    ...data.default.content,
-    title: yfmTransformInline(data.default.content.title),
-    description: yfmTransform(data.default.content.description),
-    additionalInfo: yfmTransform(data.default.content.additionalInfo),
-};
-
 const DefaultTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <PageConstructor content={{blocks: [args]}} />
+    <Header {...(blockTransform(args) as HeaderBlockProps)} />
 );
 
-const SizeTemplate: StoryFn<HeaderBlockModel> = (args) => (
+const VariantsTemplate: StoryFn<Record<number, HeaderBlockModel>> = (args) => (
     <React.Fragment>
-        <DefaultTemplate {...args} title={getSizeTitle('"L"')} width="l" />
-        <DefaultTemplate {...args} title={getSizeTitle('"M"')} width="m" />
-        <DefaultTemplate {...args} title={getSizeTitle('"S"')} width="s" />
-    </React.Fragment>
-);
-
-const CenteredTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate {...args} title={getSizeTitle('"L"')} width="l" centered />
-        <DefaultTemplate {...args} title={getSizeTitle('"M"')} width="m" centered />
-        <DefaultTemplate {...args} title={getSizeTitle('"S"')} width="s" centered />
-    </React.Fragment>
-);
-
-const ImageTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate {...args} title={getImageTitle('«M»')} width="m" />
-        <DefaultTemplate {...args} title={getImageTitle('«S»')} width="s" />
-    </React.Fragment>
-);
-
-const FitTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate {...args} title={getImageTitle('«M»')} width="m" />
-        <DefaultTemplate
-            {...args}
-            {...data.imageFit.content}
-            title={getImageTitle('«S»')}
-            width="s"
-        />
-        <DefaultTemplate {...args} {...data.video.content} title={getImageTitle('«S»')} width="s" />
-        <DefaultTemplate
-            {...args}
-            {...data.videoFit.content}
-            title={getImageTitle('«S»')}
-            width="s"
-            description={yfmTransform(data.videoFit.description)}
-        />
-    </React.Fragment>
-);
-
-const VerticalOffsetTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate {...args} title={getVerticalOffsetTitle('«0»')} verticalOffset="0" />
-        <DefaultTemplate {...args} title={getVerticalOffsetTitle('«S»')} verticalOffset="s" />
-        <DefaultTemplate {...args} title={getVerticalOffsetTitle('«M»')} verticalOffset="m" />
-        <DefaultTemplate {...args} title={getVerticalOffsetTitle('«L»')} verticalOffset="l" />
-        <DefaultTemplate {...args} title={getVerticalOffsetTitle('«XL»')} verticalOffset="xl" />
-    </React.Fragment>
-);
-
-const BackgroundTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.image.title)}
-            background={data.media.content.image.background}
-        />
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.video.title)}
-            background={data.media.content.video.background}
-        />
-    </React.Fragment>
-);
-
-const FullWithBackgroundTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.image.title)}
-            background={{
-                light: {
-                    ...data.media.content.image.background.light,
-                    fullWidth: true,
-                },
-                dark: {
-                    ...data.media.content.image.background.dark,
-                    fullWidth: true,
-                },
-            }}
-        />
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.video.title)}
-            background={{
-                light: {
-                    ...data.media.content.video.background.light,
-                    fullWidth: true,
-                },
-                dark: {
-                    ...data.media.content.video.background.dark,
-                    fullWidth: true,
-                },
-            }}
-        />
-    </React.Fragment>
-);
-
-const FullWidthMediaBackgroundTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.image.title)}
-            background={{
-                light: {
-                    ...data.media.content.image.background.light,
-                    fullWidth: true,
-                    fullWidthMedia: true,
-                },
-                dark: {
-                    ...data.media.content.image.background.dark,
-                    fullWidth: true,
-                    fullWidthMedia: true,
-                },
-            }}
-        />
-        <DefaultTemplate
-            {...args}
-            title={yfmTransformInline(data.media.content.video.title)}
-            background={{
-                light: {
-                    ...data.media.content.video.background.light,
-                    fullWidth: true,
-                    fullWidthMedia: true,
-                },
-                dark: {
-                    ...data.media.content.video.background.dark,
-                    fullWidth: true,
-                    fullWidthMedia: true,
-                },
-            }}
-        />
-    </React.Fragment>
-);
-
-const BreadCrumbsTemplate: StoryFn<HeaderBlockModel> = (args) => (
-    <React.Fragment>
-        <DefaultTemplate {...args} title={getBreadcrumbsTitle('light')} />
-        <DefaultTemplate
-            {...args}
-            title={getBreadcrumbsTitle('dark')}
-            theme={data.themeDark.content.theme as 'dark'}
-            background={data.themeDark.content.background}
-            buttons={
-                data.themeDark.content.buttons as Pick<
-                    ButtonProps,
-                    'url' | 'text' | 'theme' | 'primary' | 'size'
-                >[]
-            }
-        />
+        {Object.values(args).map((arg, index) => (
+            <div key={index} style={{marginBottom: '120px'}}>
+                <Header {...(blockTransform(arg) as HeaderBlockProps)} />
+            </div>
+        ))}
     </React.Fragment>
 );
 
 export const Default = DefaultTemplate.bind({});
-export const Size = SizeTemplate.bind({});
-export const Centered = CenteredTemplate.bind({});
-export const Image = ImageTemplate.bind({});
-export const VerticalOffset = VerticalOffsetTemplate.bind({});
-export const Background = BackgroundTemplate.bind({});
-export const FullWithBackground = FullWithBackgroundTemplate.bind({});
-export const FullWidthMediaBackground = FullWidthMediaBackgroundTemplate.bind({});
+export const Size = VariantsTemplate.bind([]);
+export const Centered = VariantsTemplate.bind([]);
+export const Image = VariantsTemplate.bind([]);
+export const VerticalOffset = VariantsTemplate.bind([]);
+export const Background = VariantsTemplate.bind([]);
+export const FullWithBackground = VariantsTemplate.bind([]);
+export const FullWidthMediaBackground = VariantsTemplate.bind({});
 export const DarkTheme = DefaultTemplate.bind({});
-export const Breadcrumbs = BreadCrumbsTemplate.bind({});
-export const MediaViewFit = FitTemplate.bind({});
-export const VideoIframe = DefaultTemplate.bind([]);
+export const Breadcrumbs = VariantsTemplate.bind([]);
+export const MediaViewFit = VariantsTemplate.bind([]);
+export const VideoIframe = DefaultTemplate.bind({});
 
-Default.args = {...DefaultArgs} as HeaderBlockProps;
+Default.args = data.default as HeaderBlockModel;
 
-Size.args = {...DefaultArgs} as HeaderBlockPropsNoTitle;
+Size.args = SIZES as HeaderBlockModel[];
+Size.parameters = {
+    controls: {
+        include: Object.keys(SIZES),
+    },
+};
 
-Centered.args = {...DefaultArgs} as HeaderBlockPropsNoTitle;
+Centered.args = SIZES.map((item) => ({...item, centered: true})) as HeaderBlockModel[];
+Centered.parameters = {
+    controls: {
+        include: Object.keys(SIZES),
+    },
+};
 
-Image.args = {
-    ...DefaultArgs,
-    ...data.image.content,
-} as HeaderBlockPropsNoTitle;
+Image.args = ['m', 's'].map((width) => ({
+    ...data.default,
+    width,
+    ...data.image,
+    title: data.image.title.replace('{{width}}', width),
+})) as HeaderBlockModel[];
+Image.parameters = {
+    controls: {
+        include: Object.keys(Image.args),
+    },
+};
 
-VerticalOffset.args = {
-    ...DefaultArgs,
-    ...data.image.content,
-} as HeaderBlockPropsNoTitle;
+VerticalOffset.args = ['0', 's', 'm', 'l', 'xl'].map((verticalOffset) => ({
+    verticalOffset,
+    ...data.default,
+    ...data.image,
+    width: 's',
+    title: data.verticalOffset.title.replace('{{offset}}', verticalOffset),
+})) as HeaderBlockModel[];
+VerticalOffset.parameters = {
+    controls: {
+        include: Object.keys(VerticalOffset.args),
+    },
+};
 
-Background.args = {
-    ...DefaultArgs,
-} as HeaderBlockPropsNoTitle;
+Background.args = [
+    {background: data.media.image.background, title: 'Block with image'},
+    {background: data.media.video.background, title: 'Block with video'},
+].map((background) => ({
+    ...data.default,
+    ...background,
+})) as HeaderBlockModel[];
+Background.parameters = {
+    controls: {
+        include: Object.keys(Background.args),
+    },
+};
 
-FullWithBackground.args = {
-    ...DefaultArgs,
-} as HeaderBlockPropsNoTitle;
+FullWithBackground.args = [data.media.image.background, data.media.video.background].map(
+    (background) => ({
+        ...data.default,
+        background: {
+            light: {...background.light, fullWidth: true},
+            dark: {...background.dark, fullWidth: true},
+        },
+    }),
+) as HeaderBlockModel[];
+FullWithBackground.parameters = {
+    controls: {
+        include: Object.keys(FullWithBackground.args),
+    },
+};
 
-FullWidthMediaBackground.args = {
-    ...DefaultArgs,
-} as HeaderBlockPropsNoTitle;
+FullWidthMediaBackground.args = [data.media.image.background, data.media.video.background].map(
+    (background) => ({
+        ...data.default,
+        background: {
+            light: {...background.light, fullWidth: true, fullWidthMedia: true},
+            dark: {...background.dark, fullWidth: true, fullWidthMedia: true},
+        },
+    }),
+) as HeaderBlockModel[];
+FullWidthMediaBackground.parameters = {
+    controls: {
+        include: Object.keys(FullWidthMediaBackground.args),
+    },
+};
 
 DarkTheme.args = {
-    ...DefaultArgs,
-    ...data.themeDark.content,
+    ...data.default,
+    ...data.themeDark,
 } as HeaderBlockProps;
 
-Breadcrumbs.args = {
-    ...DefaultArgs,
-    ...data.breadcrumbs.content,
-} as HeaderBlockProps;
+Breadcrumbs.args = data.breadcrumbs as HeaderBlockModel[];
+Breadcrumbs.parameters = {
+    controls: {
+        include: Object.keys(Breadcrumbs.args),
+    },
+};
 
-MediaViewFit.args = {
-    ...DefaultArgs,
-    ...data.image.content,
-    mediaView: 'fit',
-} as HeaderBlockProps;
+MediaViewFit.args = data.fit as HeaderBlockModel[];
+MediaViewFit.parameters = {
+    controls: {
+        include: Object.keys(MediaViewFit.args),
+    },
+};
 
 VideoIframe.args = {
-    ...DefaultArgs,
-    ...data.videoIframe.content,
-} as HeaderBlockProps;
+    ...data.default,
+    ...data.videoIframe,
+} as HeaderBlockModel;
