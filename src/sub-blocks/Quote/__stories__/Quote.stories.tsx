9815229f8923bdabc848531cd010094bd2ec806e
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2025-12-12 11:02:34 +0300

    chore(quote): update story (#1353)
    * chore(quote): update story
    
    * chore(йгщеу): update story
    
    * chore(Quote): fix code after review
    
    ---------
    
    Co-authored-by: Petr Krotov <peterkrotov@peterkrotov-dev.vla.yp-c.yandex.net>

--- src/sub-blocks/Quote/__stories__/Quote.stories.tsx
+++ src/sub-blocks/Quote/__stories__/Quote.stories.tsx
@@ -1,7 +1,7 @@
 import {Meta, StoryFn} from '@storybook/react';
 
-import {yfmTransformInline} from '../../../../.storybook/utils';
-import {QuoteProps, QuoteType} from '../../../models';
+import {blockTransform} from '../../../../.storybook/utils';
+import {QuoteModel, QuoteProps, QuoteType} from '../../../models';
 import Quote from '../Quote';
 
 import data from './data.json';
@@ -9,38 +9,56 @@ import data from './data.json';
 export default {
     title: 'Components/Cards/Quote',
     component: Quote,
-    args: {
-        border: 'shadow',
-        theme: 'light',
+    argTypes: {
+        color: {control: 'color'},
     },
-    argTypes: {color: {control: 'color'}},
 } as Meta;
 
-const DefaultTemplate: StoryFn<QuoteProps> = (args) => (
+const DefaultTemplate: StoryFn<QuoteModel> = (args) => (
     <div style={{maxWidth: '1248px'}}>
-        <Quote {...args} />
+        <Quote {...(blockTransform(args) as QuoteProps)} />
     </div>
 );
-const QuoteTypesTemplate: StoryFn<QuoteProps> = (args) => (
+
+const QuoteTypesTemplate: StoryFn<Record<string, QuoteModel>> = (args) => (
     <div style={{maxWidth: '1248px', display: 'flex', flexDirection: 'column', gap: '24px'}}>
-        <Quote {...args} quoteType={QuoteType.Chevron} />
-        <Quote {...args} quoteType={QuoteType.EnglishDouble} />
+        {Object.entries(args)
+            .map(([key, item]) => {
+                const transformedArgs = blockTransform(item) as QuoteProps;
+                return <Quote key={key} {...transformedArgs} />;
+            })
+            .filter(Boolean)}
     </div>
 );
+
 export const Default = DefaultTemplate.bind({});
 export const QuoteTypes = QuoteTypesTemplate.bind({});
 export const BorderLine = DefaultTemplate.bind({});
 export const DarkTheme = DefaultTemplate.bind({});
 
-const DefaultArgs = {
-    ...data.default.content,
-    yfmText: yfmTransformInline(data.default.content.yfmText),
-} as QuoteProps;
+Default.args = data.default.content as QuoteModel;
 
-Default.args = DefaultArgs;
-QuoteTypes.args = DefaultArgs;
 BorderLine.args = {
-    ...DefaultArgs,
+    ...data.default.content,
     ...data.borderLine.content,
-} as QuoteProps;
-DarkTheme.args = data.darkTheme.content as QuoteProps;
+} as QuoteModel;
+
+DarkTheme.args = data.darkTheme.content as QuoteModel;
+
+const QUOTE_TYPES: Record<string, QuoteModel> = {
+    0: {
+        ...data.default.content,
+        quoteType: QuoteType.Chevron,
+    } as QuoteModel,
+    1: {
+        ...data.default.content,
+        quoteType: QuoteType.EnglishDouble,
+    } as QuoteModel,
+};
+
+QuoteTypes.args = QUOTE_TYPES;
+QuoteTypes.parameters = {
+    controls: {
+        include: Object.keys(QUOTE_TYPES),
+    },
+};
