2c856e901625a2f2d7d38ae6269acf66bc57d605
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2026-02-10 11:09:17 +0300

    feat(Tabs): update config, story and screenshots (#1350)
    * feat(Tabs): update config, story and screenshots
    
    * chore(Tabs): update story
    
    * chore(Tabs): update story

--- src/text-transform/config.ts
+++ src/text-transform/config.ts
@@ -352,6 +352,12 @@ export const config: BlocksConfig = {
             parser: parseItemsTitle,
             renderInline: true,
         },
+        {
+            fields: ['items'],
+            transformer: yfmTransformer,
+            parser: createItemsParser(['list.text']),
+            renderInline: true,
+        },
     ],
     [BlockType.TableBlock]: [
         {
