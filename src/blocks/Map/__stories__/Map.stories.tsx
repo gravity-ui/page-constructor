f6b3f168e80e17ed7cd9bbfbf09182808df6d86c
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2026-02-14 10:28:09 +0300

    chore(Map): update map storybook (#1347)
    * chore(Map): update map storybook
    
    * chore(Map): update story
    
    * chore(Map): update story and refresh PR

--- src/blocks/Map/__stories__/Map.stories.tsx
+++ src/blocks/Map/__stories__/Map.stories.tsx
@@ -3,12 +3,11 @@ import * as React from 'react';
 import {Meta, StoryFn} from '@storybook/react';
 
 import {scriptsSrc, ymapApiKeyForStorybook} from '../../../../.storybook/maps';
-import {yfmTransform} from '../../../../.storybook/utils';
+import {blockTransform} from '../../../../.storybook/utils';
 import {ApiKeyInput} from '../../../components/Map/__stories__/ApiKeyInput';
-import {PageConstructor} from '../../../containers/PageConstructor';
 import {MapType} from '../../../context/mapsContext/mapsContext';
 import {MapProvider, gmapApiKeyIdInLS} from '../../../context/mapsContext/mapsProvider';
-import {ButtonProps, LinkProps, MapBlockModel, MapBlockProps} from '../../../models';
+import {MapBlockModel, MapBlockProps} from '../../../models';
 import MapBlock from '../Map';
 
 import data from './data.json';
@@ -16,11 +15,6 @@ import data from './data.json';
 export default {
     title: 'Blocks/Map',
     component: MapBlock,
-    args: {
-        largeMedia: false,
-        mediaOnly: false,
-        size: 'l',
-    },
 } as Meta;
 
 const DefaultTemplate: StoryFn<MapBlockModel> = (args) => (
@@ -29,213 +23,258 @@ const DefaultTemplate: StoryFn<MapBlockModel> = (args) => (
         apiKey={ymapApiKeyForStorybook}
         type={MapType.Yandex}
     >
-        <PageConstructor
-            content={{
-                blocks: [
-                    {
-                        ...args,
-                        additionalInfo: yfmTransform(data.common.additionalInfo),
-                        map: data.ymap,
-                    },
-                    {
-                        ...args,
-                        links: data.common.links as LinkProps[],
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-2',
-                        },
-                    },
-                    {
-                        ...args,
-                        buttons: data.common.buttons as ButtonProps[],
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-3',
-                        },
-                    },
-                ],
-            }}
-        />
+        <div style={{padding: '64px'}}>
+            <MapBlock {...(blockTransform(args) as MapBlockProps)} />
+        </div>
     </MapProvider>
 );
 
-const SizeTemplate: StoryFn<MapBlockModel> = (args) => (
+const MultipleTemplate: StoryFn<Record<number, MapBlockModel>> = (args) => (
     <MapProvider
         scriptSrc={scriptsSrc[MapType.Yandex]}
         apiKey={ymapApiKeyForStorybook}
         type={MapType.Yandex}
     >
-        <PageConstructor
-            content={{
-                blocks: [
-                    {
-                        ...args,
-                        title: data.size.defaultMediaTitle,
-                        map: data.ymap,
-                    },
-                    {
-                        ...args,
-                        largeMedia: true,
-                        title: data.size.largeMediaTitle,
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-2',
-                        },
-                    },
-                    {
-                        ...args,
-                        mediaOnly: true,
-                        description: undefined,
-                        title: data.size.mediaOnlyTitle,
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-3',
-                        },
-                    },
-                ],
-            }}
-        />
-    </MapProvider>
-);
-
-const DirectionTemplate: StoryFn<MapBlockModel> = (args) => (
-    <MapProvider
-        scriptSrc={scriptsSrc[MapType.Yandex]}
-        apiKey={ymapApiKeyForStorybook}
-        type={MapType.Yandex}
-    >
-        <PageConstructor
-            content={{
-                blocks: [
-                    {
-                        ...args,
-                        title: data.direction.defaultDirectionTitle,
-                        map: data.ymap,
-                    },
-                    {
-                        ...args,
-                        title: data.direction.ReverseDirectionTitle,
-                        direction: 'media-content',
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-2',
-                        },
-                    },
-                    {
-                        ...args,
-                        title: data.direction.ReverseDirectionTitle,
-                        mobileDirection: 'media-content',
-                        map: {
-                            ...data.ymap,
-                            id: 'common-places-3',
-                        },
-                    },
-                ],
-            }}
-        />
+        <div style={{padding: '64px', display: 'flex', gap: 20, flexDirection: 'column'}}>
+            {Object.values(args).map((item, index) => (
+                <MapBlock key={index} {...(blockTransform(item) as MapBlockProps)} />
+            ))}
+        </div>
     </MapProvider>
 );
 
 const GMAP_API_KEY = process.env.STORYBOOK_GMAP_API_KEY;
 
-const MapsTypesTemplate: StoryFn<MapBlockModel> = (args) => (
-    <React.Fragment>
-        <MapProvider
-            scriptSrc={scriptsSrc[MapType.Yandex]}
-            apiKey={ymapApiKeyForStorybook}
-            type={MapType.Yandex}
-        >
-            <PageConstructor
-                content={{
-                    blocks: [
-                        {
-                            ...args,
-                            title: data.direction.defaultDirectionTitle,
-                            map: data.ymap,
-                        },
-                    ],
-                }}
-            />
-        </MapProvider>
-        <MapProvider
-            scriptSrc={scriptsSrc[MapType.Google]}
-            type={MapType.Google}
-            apiKey={GMAP_API_KEY}
-        >
-            {!GMAP_API_KEY && (
-                <div style={{maxWidth: '500px', marginLeft: '40px'}}>
-                    <ApiKeyInput id={gmapApiKeyIdInLS} />
-                </div>
-            )}
-            <PageConstructor
-                content={{
-                    blocks: [
-                        {
-                            ...args,
-                            title: data.direction.ReverseDirectionTitle,
-                            direction: 'media-content',
-                            map: data.gmap,
-                        },
-                    ],
-                }}
-            />
-        </MapProvider>
-    </React.Fragment>
-);
+const MapsTypesTemplate: StoryFn<Record<number, MapBlockModel>> = (args) => {
+    const items = Object.values(args);
+    const yandexMap = items[0];
+    const googleMap = items[1];
 
-const EnhancedTitleTemplate: StoryFn<MapBlockModel> = (args) => (
+    return (
+        <React.Fragment>
+            {yandexMap && (
+                <MapProvider
+                    scriptSrc={scriptsSrc[MapType.Yandex]}
+                    apiKey={ymapApiKeyForStorybook}
+                    type={MapType.Yandex}
+                >
+                    <MapBlock {...(blockTransform(yandexMap) as MapBlockProps)} />
+                </MapProvider>
+            )}
+            {googleMap && (
+                <MapProvider
+                    scriptSrc={scriptsSrc[MapType.Google]}
+                    type={MapType.Google}
+                    apiKey={GMAP_API_KEY}
+                >
+                    {!GMAP_API_KEY && (
+                        <div style={{maxWidth: '500px', marginLeft: '40px'}}>
+                            <ApiKeyInput id={gmapApiKeyIdInLS} />
+                        </div>
+                    )}
+                    <MapBlock {...(blockTransform(googleMap) as MapBlockProps)} />
+                </MapProvider>
+            )}
+        </React.Fragment>
+    );
+};
+
+const EnhancedTitleTemplate: StoryFn<Record<number, MapBlockModel>> = (args) => (
     <MapProvider
         scriptSrc={scriptsSrc[MapType.Yandex]}
         apiKey={ymapApiKeyForStorybook}
         type={MapType.Yandex}
     >
-        <PageConstructor
-            content={{
-                blocks: [
-                    {
-                        ...args,
-                        ...data.enhancedTitle.largeClickable,
-                        map: data.ymap,
-                    },
-                    {
-                        ...args,
-                        ...data.enhancedTitle.interactiveWithIcon,
-                        title: {
-                            ...data.enhancedTitle.interactiveWithIcon.title,
-                            onClick: () => alert('Map title clicked!'),
-                        },
-                        map: {
-                            ...data.ymap,
-                            id: 'interactive-title-map',
-                        },
-                    },
-                ],
-            }}
-        />
+        <div style={{padding: '64px', display: 'flex', gap: 20, flexDirection: 'column'}}>
+            {Object.values(args).map((item, index) => {
+                const transformedItem =
+                    index === 1
+                        ? {
+                              ...item,
+                              title: {
+                                  ...(typeof item.title === 'object' && item.title !== null
+                                      ? item.title
+                                      : {}),
+                                  onClick: () => alert('Map title clicked!'),
+                              },
+                          }
+                        : item;
+
+                return (
+                    <MapBlock key={index} {...(blockTransform(transformedItem) as MapBlockProps)} />
+                );
+            })}
+        </div>
     </MapProvider>
 );
 
 export const Default = DefaultTemplate.bind({});
-export const Size = SizeTemplate.bind({});
-export const Direction = DirectionTemplate.bind({});
-export const MapsTypes = MapsTypesTemplate.bind({});
-export const EnhancedTitle = EnhancedTitleTemplate.bind({});
+export const WithControls = MultipleTemplate.bind([]);
+export const Size = MultipleTemplate.bind([]);
+export const Direction = MultipleTemplate.bind([]);
+export const MapsTypes = MapsTypesTemplate.bind([]);
+export const EnhancedTitle = EnhancedTitleTemplate.bind([]);
 
-const DefaultArgs = {
+Default.args = {
     ...data.default.content,
     title: data.common.title,
-    description: yfmTransform(data.common.description),
+    description: data.common.description,
     map: data.ymap,
+} as MapBlockModel;
+
+const CONTROLS_MAPS: MapBlockModel[] = [
+    {
+        ...data.default.content,
+        title: data.common.title,
+        description: data.common.description,
+        additionalInfo: data.common.additionalInfo,
+        map: data.ymap,
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        title: data.common.title,
+        description: data.common.description,
+        links: data.common.links,
+        map: {
+            ...data.ymap,
+            id: 'common-places-2',
+        },
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        title: data.common.title,
+        description: data.common.description,
+        buttons: data.common.buttons,
+        map: {
+            ...data.ymap,
+            id: 'common-places-3',
+        },
+    } as MapBlockModel,
+];
+
+WithControls.args = CONTROLS_MAPS;
+WithControls.parameters = {
+    controls: {
+        include: Object.keys(CONTROLS_MAPS),
+    },
 };
 
-Default.args = DefaultArgs as MapBlockProps;
+const SIZE_MAPS: MapBlockModel[] = [
+    {
+        ...data.default.content,
+        title: data.size.defaultMediaTitle,
+        description: data.common.description,
+        map: data.ymap,
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        largeMedia: true,
+        title: data.size.largeMediaTitle,
+        description: data.common.description,
+        map: {
+            ...data.ymap,
+            id: 'common-places-2',
+        },
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        mediaOnly: true,
+        description: undefined,
+        title: data.size.mediaOnlyTitle,
+        map: {
+            ...data.ymap,
+            id: 'common-places-3',
+        },
+    } as MapBlockModel,
+];
 
-Size.args = DefaultArgs as MapBlockProps;
-Direction.args = DefaultArgs as MapBlockProps;
+Size.args = SIZE_MAPS;
+Size.parameters = {
+    controls: {
+        include: Object.keys(SIZE_MAPS),
+    },
+};
 
-MapsTypes.args = DefaultArgs as MapBlockProps;
+const DIRECTION_MAPS: MapBlockModel[] = [
+    {
+        ...data.default.content,
+        title: data.direction.defaultDirectionTitle,
+        description: data.common.description,
+        map: data.ymap,
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        title: data.direction.ReverseDirectionTitle,
+        description: data.common.description,
+        direction: 'media-content',
+        map: {
+            ...data.ymap,
+            id: 'common-places-2',
+        },
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        title: data.direction.ReverseDirectionTitle,
+        description: data.common.description,
+        mobileDirection: 'media-content',
+        map: {
+            ...data.ymap,
+            id: 'common-places-3',
+        },
+    } as MapBlockModel,
+];
 
-EnhancedTitle.args = {
-    ...DefaultArgs,
-    ...data.enhancedTitle.largeClickable,
-} as MapBlockProps;
+Direction.args = DIRECTION_MAPS;
+Direction.parameters = {
+    controls: {
+        include: Object.keys(DIRECTION_MAPS),
+    },
+};
+
+const MAPS_TYPES: MapBlockModel[] = [
+    {
+        ...data.default.content,
+        title: data.direction.defaultDirectionTitle,
+        description: data.common.description,
+        map: data.ymap,
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        title: data.direction.ReverseDirectionTitle,
+        description: data.common.description,
+        direction: 'media-content',
+        map: data.gmap,
+    } as MapBlockModel,
+];
+
+MapsTypes.args = MAPS_TYPES;
+MapsTypes.parameters = {
+    controls: {
+        include: Object.keys(MAPS_TYPES),
+    },
+};
+
+const ENHANCED_TITLE_MAPS: MapBlockModel[] = [
+    {
+        ...data.default.content,
+        ...data.enhancedTitle.largeClickable,
+        description: data.common.description,
+        map: data.ymap,
+    } as MapBlockModel,
+    {
+        ...data.default.content,
+        ...data.enhancedTitle.interactiveWithIcon,
+        description: data.common.description,
+        map: {
+            ...data.ymap,
+            id: 'interactive-title-map',
+        },
+    } as MapBlockModel,
+];
+
+EnhancedTitle.args = ENHANCED_TITLE_MAPS;
+EnhancedTitle.parameters = {
+    controls: {
+        include: Object.keys(ENHANCED_TITLE_MAPS),
+    },
+};
--- src/blocks/Map/__tests__/Map.visual.test.tsx
+++ src/blocks/Map/__tests__/Map.visual.test.tsx
@@ -1,12 +1,18 @@
 import {test} from '../../../../playwright/core/index';
 
-import {Default, Direction, MapsTypes, Size} from './helpers';
+import {Default, Direction, MapsTypes, Size, WithControls} from './helpers';
 
 const DEFAULT_MAP_DELAY = 10 * 1000;
 
 test.describe('Map', () => {
     test('render stories <Default>', async ({mount, expectScreenshot, delay, page}) => {
         await mount(<Default />);
+        await delay(DEFAULT_MAP_DELAY);
+        await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
+    });
+
+    test('render stories <WithControls>', async ({mount, expectScreenshot, delay, page}) => {
+        await mount(<WithControls />);
         await delay(DEFAULT_MAP_DELAY);
         await expectScreenshot({skipTheme: 'dark', mask: [page.locator('.pc-map')]});
     });
