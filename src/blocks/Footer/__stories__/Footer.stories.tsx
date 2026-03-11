b1159927c57b0d06d6f64f5b35930eed6226f5db
Igor Dyrov <dyrovigor@yandex-team.ru>
2026-03-11 12:54:41 +0300

    clean styles and stories

--- src/blocks/Footer/__stories__/Footer.stories.tsx
+++ src/blocks/Footer/__stories__/Footer.stories.tsx
@@ -17,21 +17,7 @@ const DefaultTemplate: StoryFn<FooterBlockModel> = (args) => (
     <Footer {...(blockTransform(args) as FooterBlockProps)} />
 );
 
-const VariantsTemplate: StoryFn<Record<string, FooterBlockModel>> = (args) => (
-    <React.Fragment>
-        {Object.values(args).map((arg, index) => (
-            <div key={index} style={{marginBottom: '48px'}}>
-                <Footer {...(blockTransform(arg) as FooterBlockProps)} />
-            </div>
-        ))}
-    </React.Fragment>
-);
-
 export const Full = DefaultTemplate.bind({});
-export const Default = DefaultTemplate.bind({});
-export const WithBackground = DefaultTemplate.bind({});
-export const ColumnsOnly = DefaultTemplate.bind({});
-export const AllVariants = VariantsTemplate.bind({});
 
 Full.args = data.full as FooterBlockModel;
 Full.parameters = {
@@ -39,14 +25,5 @@ Full.parameters = {
         description: {
             story: 'Full 5-floor footer: (1) logo + link columns + additional row, (2) Join Us + social icons, (3) legal disclaimer, (4) privacy/terms + language + copyright, (5) attribution.',
         },
-    },
-};
-Default.args = data.default as FooterBlockModel;
-WithBackground.args = data.withBackground as FooterBlockModel;
-ColumnsOnly.args = data.columnsOnly as FooterBlockModel;
-AllVariants.args = data as Record<string, FooterBlockModel>;
-AllVariants.parameters = {
-    controls: {
-        include: Object.keys(data),
     },
 };
