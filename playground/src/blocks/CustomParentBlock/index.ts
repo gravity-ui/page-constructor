1fff057f8b118c394f8786eb5fcfb710a3a3d2a0
aeksandla <aeksandla@yandex-team.ru>
2026-04-01 11:11:03 +0300

    chore: fix

--- playground/src/blocks/CustomParentBlock/index.ts
+++ playground/src/blocks/CustomParentBlock/index.ts
@@ -9,15 +9,13 @@ const CustomParentBlockConfig: BlockData = {
         name: 'Custom Parent Block',
         group: 'custom',
         inputs: [
-            {type: 'text', name: 'title', title: 'Title'},
-            {type: 'textarea', name: 'description', title: 'Description'},
+            {type: 'textInput', name: 'title', title: 'Title'},
+            {type: 'textArea', name: 'description', title: 'Description'},
             {
                 type: 'select',
                 name: 'perRow',
                 title: 'Cards per row',
-                view: 'radiobutton',
-                mode: 'single',
-                enum: [
+                options: [
                     {value: '3', content: '3'},
                     {value: '4', content: '4'},
                     {value: '5', content: '5'},
