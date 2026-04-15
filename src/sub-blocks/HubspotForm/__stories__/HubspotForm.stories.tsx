db4f1603cf331d8d2e7734b967d001591e2866ed
lentsd <lentsd@yandex-team.ru>
2026-04-15 12:56:55 +0300

    fix(HubspotForm): improve storybook story controls

--- src/sub-blocks/HubspotForm/__stories__/HubspotForm.stories.tsx
+++ src/sub-blocks/HubspotForm/__stories__/HubspotForm.stories.tsx
@@ -3,6 +3,8 @@ import {Meta, StoryFn} from '@storybook/react';
 import HubspotForm from '..';
 import {HubspotFormProps} from '../../../models/';
 
+import data from './data.json';
+
 export default {
     component: HubspotForm,
     title: 'Components/HubspotForm',
@@ -16,16 +18,4 @@ const DefaultTemplate: StoryFn<HubspotFormProps> = (args) => (
 
 export const Default = DefaultTemplate.bind({});
 
-Default.args = {
-    region: 'eu1',
-    portalId: '25764979',
-    formId: 'a3eb06a6-e8ce-45d4-81bd-7fadb7dab313',
-    className: 'example-class-name',
-    /*eslint-disable  no-console */
-    onBeforeLoad: () => console.log('onBeforeLoad'),
-    onBeforeSubmit: () => console.log('onBeforeSubmit'),
-    onSubmitError: () => console.log('onSubmitError'),
-    onLoad: () => console.log('onLoad'),
-    onSubmit: () => console.log('onSubmit'),
-    /*eslint-enable */
-};
+Default.args = data.default as HubspotFormProps;
