1270903102502a936d2d318547a3132250eac701
Daniel <64872689+lentsd@users.noreply.github.com>
2026-04-15 13:32:04 +0300

    fix(Title): improve storybook story controls (#1412)
    * fix(Title): improve storybook story controls

--- src/components/Title/__stories__/Title.stories.tsx
+++ src/components/Title/__stories__/Title.stories.tsx
@@ -6,65 +6,91 @@ import Title, {TitleProps} from '../Title';
 
 import data from './data.json';
 
+type SizeWithSubtitleProps = TitleItemProps & {subtitle: string};
+
 export default {
     component: Title,
     title: 'Components/Title',
 } as Meta;
 
-const DefaultTemplate: StoryFn<TitleProps & ClassNameProps> = (args) => <Title {...args} />;
+const DefaultTemplate: StoryFn<TitleProps & ClassNameProps> = (args) => {
+    const transformedSubtitle = args.subtitle ? yfmTransform(args.subtitle) : undefined;
 
-const SizesTemplate: StoryFn<TitleProps & ClassNameProps> = (args) => {
-    const titleItemObjectProps = typeof args.title === 'object' ? args.title : {};
-
-    return (
-        <div>
-            {Object.entries(data.sizes).map(([size, props]) => (
-                <div key={size} style={{paddingBottom: '64px'}}>
-                    <Title
-                        {...args}
-                        title={
-                            {
-                                ...props,
-                                ...titleItemObjectProps,
-                            } as TitleItemProps
-                        }
-                    />
-                </div>
-            ))}
-        </div>
-    );
+    return <Title {...args} subtitle={transformedSubtitle} />;
 };
 
-const DefaultArgs = {
-    ...data.default.content,
-    subtitle: yfmTransform(data.default.content.subtitle),
-};
+const SizesTemplate: StoryFn<Record<string, TitleItemProps>> = (args) => (
+    <div>
+        {Object.values(args).map((titleProps, index) => (
+            <div key={index} style={{paddingBottom: '64px'}}>
+                <Title title={titleProps as TitleItemProps} />
+            </div>
+        ))}
+    </div>
+);
+
+const SizesWithSubtitleTemplate: StoryFn<Record<string, SizeWithSubtitleProps>> = (args) => (
+    <div>
+        {Object.values(args).map(({subtitle, ...titleProps}, index) => (
+            <div key={index} style={{paddingBottom: '64px'}}>
+                <Title
+                    title={titleProps as TitleItemProps}
+                    subtitle={subtitle ? yfmTransform(subtitle) : undefined}
+                />
+            </div>
+        ))}
+    </div>
+);
+
+const sizesWithLinks = Object.fromEntries(
+    Object.entries(data.sizes).map(([size, props]) => [
+        size,
+        {
+            ...props,
+            url: 'https://example.com',
+            urlTitle: 'Example website. Opens in a new window',
+        },
+    ]),
+);
 
 export const Default = DefaultTemplate.bind({});
-export const TitleLink = DefaultTemplate.bind({});
 export const CustomTitle = DefaultTemplate.bind({});
+export const TitleLink = DefaultTemplate.bind({});
 export const Sizes = SizesTemplate.bind({});
 export const SizesWithLinks = SizesTemplate.bind({});
+export const SizesWithSubtitle = SizesWithSubtitleTemplate.bind({});
 export const TitleWithoutDescription = SizesTemplate.bind({});
 
-Default.args = {
-    ...DefaultArgs,
-} as TitleProps;
-TitleLink.args = {
-    ...DefaultArgs,
-    title: data.titleLink.content.title,
-} as TitleProps;
-CustomTitle.args = {
-    ...DefaultArgs,
-    title: data.customTitle.content.title,
-} as TitleProps;
-Sizes.args = {
-    ...DefaultArgs,
-} as TitleProps;
-SizesWithLinks.args = {
-    ...DefaultArgs,
-    title: data.titleLink.content.title,
-} as TitleProps;
-TitleWithoutDescription.args = {
-    title: data.default.content.title,
-} as TitleProps;
+Default.args = data.default as TitleProps;
+
+CustomTitle.args = data.customTitle as TitleProps;
+
+TitleLink.args = data.titleLink as TitleProps;
+
+Sizes.args = data.sizes as Record<string, TitleItemProps>;
+Sizes.parameters = {
+    controls: {
+        include: Object.keys(data.sizes),
+    },
+};
+
+SizesWithLinks.args = sizesWithLinks as Record<string, TitleItemProps>;
+SizesWithLinks.parameters = {
+    controls: {
+        include: Object.keys(sizesWithLinks),
+    },
+};
+
+SizesWithSubtitle.args = data.sizesWithSubtitle as Record<string, SizeWithSubtitleProps>;
+SizesWithSubtitle.parameters = {
+    controls: {
+        include: Object.keys(data.sizesWithSubtitle),
+    },
+};
+
+TitleWithoutDescription.args = data.sizes as Record<string, TitleItemProps>;
+TitleWithoutDescription.parameters = {
+    controls: {
+        include: Object.keys(data.sizes),
+    },
+};
