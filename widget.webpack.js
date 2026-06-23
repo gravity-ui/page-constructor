59a8251014697bb98800b16d38e052f9ef44d3ff
Evgenii Alaev <alaev89@yandex-team.ru>
2026-06-23 11:45:40 +0200

    fix(deps): bump swiper to ^12.2.0 to fix prototype pollution (CVE-2026-27212)
    Swiper 12 ships CSS with native nesting; the widget build now processes
    plain vendor CSS via css-loader + postcss-nesting instead of sass-loader.

--- widget.webpack.js
+++ widget.webpack.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const autoprefixer = require('autoprefixer');
+const postcssNesting = require('postcss-nesting');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const SRC_PATH = path.resolve('src');
@@ -10,6 +11,15 @@ const NODE_MODULES_PATH = path.resolve('node_modules');
 const WIDGET_SRC_PATH = path.resolve(SRC_PATH, 'widget');
 const WIDGET_RESULT_PATH = path.resolve(__dirname, 'widget');
 const WIDGET_BUNDLE_FILENAME = 'index.js';
+
+const STYLE_INCLUDE_PATHS = [
+    path.resolve(NODE_MODULES_PATH, '@diplodoc/transform'),
+    path.resolve(NODE_MODULES_PATH, '@gravity-ui/components'),
+    path.resolve(NODE_MODULES_PATH, '@gravity-ui/uikit'),
+    path.resolve(NODE_MODULES_PATH, 'swiper'),
+    path.resolve(__dirname, 'styles'),
+    SRC_PATH,
+];
 
 module.exports = {
     entry: {
@@ -31,15 +41,8 @@ module.exports = {
                 },
             },
             {
-                test: /\.(scss|css)$/,
-                include: [
-                    path.resolve(NODE_MODULES_PATH, '@diplodoc/transform'),
-                    path.resolve(NODE_MODULES_PATH, '@gravity-ui/components'),
-                    path.resolve(NODE_MODULES_PATH, '@gravity-ui/uikit'),
-                    path.resolve(NODE_MODULES_PATH, 'swiper'),
-                    path.resolve(__dirname, 'styles'),
-                    SRC_PATH,
-                ],
+                test: /\.scss$/,
+                include: STYLE_INCLUDE_PATHS,
                 use: [
                     'style-loader',
                     'css-loader',
@@ -51,6 +54,23 @@ module.exports = {
                     },
                     'resolve-url-loader',
                     'sass-loader',
+                ],
+            },
+            {
+                // Swiper 12+ (and other vendors) ship plain CSS that relies on
+                // native CSS nesting, which sass-loader cannot parse. Process
+                // plain CSS without sass-loader and flatten nesting via postcss.
+                test: /\.css$/,
+                include: STYLE_INCLUDE_PATHS,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {plugins: [postcssNesting(), autoprefixer()]},
+                        },
+                    },
                 ],
             },
         ],
